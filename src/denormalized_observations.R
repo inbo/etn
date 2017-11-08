9e322fed3a4f7ee72fc03fc0233c4acaf104ab2c
Peter Desmet <peter.desmet.work@gmail.com>
2017-11-08 14:38:46 +0100

    Update comments in denorm script

--- src/denormalized_observations.R
+++ src/denormalized_observations.R
@@ -1,18 +1,17 @@
+# Script to download denormalized detection data from the OTN database at VLIZ
+# Note: this script will only work on the Rstudio server environment at VLIZ
 #
-# Denormalized data download from the VLIZ server
-# -> only works on the Rstudio server installation of VLIZ
-#
-
+# Load libraries:
 library(RODBC)
 library(readr)
 
-# Read credentials of the database connection
+# Read credentials of the database connection:
 pgcreds <- scan("./.pgpass", what="")
 
-# Read the SQL query
 denorm_query <- read_file("./data_publication/denormalized_observations.sql")
+# Read SQL query file:
 
-# Connect to dbase and execute string
+# Connect to database and execute string:
 data_connect <- odbcConnect("ETN", pgcreds[1], pgcreds[2])
 odbcQuery(channel = data_connect, query = denorm_query)
 tabel <- sqlGetResults(channel = data_connect)
@@ -20,5 +19,5 @@ mess <- odbcGetErrMsg(data_connect)
 print(mess)
 close(data_connect)
 
-# Store data as CSV table
 write.csv(tabel, "./data_publication/detections_normalized.csv", quote = FALSE)
+# Save data as a CSV file:
