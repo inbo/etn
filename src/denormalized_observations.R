f84356c09188c5782d7ef8dd24249d9750606720
Peter Desmet <peter.desmet.work@gmail.com>
2017-11-08 14:40:19 +0100

    Update paths

--- src/denormalized_observations.R
+++ src/denormalized_observations.R
@@ -8,16 +8,16 @@ library(readr)
 # Read credentials of the database connection:
 pgcreds <- scan("./.pgpass", what="")
 
-denorm_query <- read_file("./data_publication/denormalized_observations.sql")
 # Read SQL query file:
+denorm_query <- read_file("denormalized_observations.sql")
 
 # Connect to database and execute string:
 data_connect <- odbcConnect("ETN", pgcreds[1], pgcreds[2])
 odbcQuery(channel = data_connect, query = denorm_query)
-tabel <- sqlGetResults(channel = data_connect)
+table <- sqlGetResults(channel = data_connect)
 mess <- odbcGetErrMsg(data_connect)
 print(mess)
 close(data_connect)
 
-write.csv(tabel, "./data_publication/detections_normalized.csv", quote = FALSE)
 # Save data as a CSV file:
+write.csv(table, "../data/raw/denormalized_observations.csv", quote = FALSE)
