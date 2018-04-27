48aefab1554503ca1592a874f47ca4263229af3f
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-27 09:52:47 +0200

    Add example of the possible inputs to header

--- R/get_receivers.R
+++ R/get_receivers.R
@@ -1,5 +1,8 @@
 # receivers  (no view)
 
+# netwerk_project = NULL  # all projects
+# netwerk_project = "my_project"  # single project
+# netwerk_project = c("my_project1", "my_project2")  # multiple project
 get_receivers <- function(connection,
                           netwerk_project = NULL) {
   NULL
