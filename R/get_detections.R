ca1ad415c14f4476d1cd620e7983964677a52741
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-27 18:13:23 +0200

    Add support for network and animal project subset on detections

--- R/get_detections.R
+++ R/get_detections.R
@@ -7,14 +7,49 @@
 #   if("All" %in% tagprojectlist) tagprojectlist=NULL
 
 
-get_detections <- function(connection,
-                           netwerk_project = NULL,
-                           animal_project = NULL,
-                           start_date = NULL,
-                           end_date = Inf,
-                           station = -Inf,
-                           tags = NULL) {
-  NULL
+get_detections <- function(connection, network_project = NULL,
+                           animal_project = NULL, start_date = NULL,
+                           end_date = NULL, station = NULL,
+                           tags = NULL, limit = 100) {
+  check_connection(connection)
+
+  # check the network project inputs
+  valid_network_projects <- get_projects(connection, project_type = "network") %>%
+    pull(projectcode)
+  check_null_or_value(network_project, valid_network_projects, "network_project")
+  if (is.null(network_project)) {
+    network_project = valid_network_projects
+  }
+
+  # check the animal project inputs
+  valid_animal_projects <- get_projects(connection, project_type = "animal") %>%
+    pull(projectcode)
+  check_null_or_value(animal_project,  valid_animal_projects, "animal_project")
+  if (is.null(animal_project)) {
+    animal_project = valid_animal_projects
+  }
+
+  # check the start- and enddate inputs
+
+  # check the station inputs
+
+  # check the tags inputs
+
+  # check the limit input
+  assert_that(is.number(limit))
+
+  detections_query <- glue_sql(
+    "SELECT * FROM vliz.detections_view
+    WHERE network_project_code IN ({network_project*})
+      AND animal_project_code IN ({animal_project*})
+    LIMIT 100",
+    network_project = network_project,
+    animal_project = animal_project,
+    .con = connection
+  )
+  detections <- dbGetQuery(connection, detections_query)
+  detections
+
 
   # mainquery <- "
   #   SELECT * from vliz.detections_view
@@ -25,19 +60,3 @@ get_detections <- function(connection,
   # sqlExecute(connection, mainquery, data, fetch = TRUE)
 
 }
-
-# {
-# wherecode=""
-# # Notice that the 'detections_view' has a different structure + is not a materialized view, which is the reason for
-# # the slightly different approach of building the 'where' clause.
-# if(!is.null(projectlist)){
-#   wherecode <- paste(wherecode," and network_project_name in ( #project# )");
-#   wherecode <- str_replace_all(wherecode,"#project#",paste("'",projectlist,"'",sep="",collapse=","));
-# }
-# if(!is.null(tagprojectlist)){
-#     wherecode <- paste(wherecode," and animal_project_name in ( #project# )");
-#     wherecode <- paste(wherecode," and tagproj.name in ( #project# )");
-# if(!logged){
-#   wherecode <- paste(wherecode," and (network_moratorium is null or network_moratorium = FALSE ) ");
-# }
-
