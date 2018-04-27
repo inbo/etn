1bad465b99ea9a43e0b9a57dad478133d4215e54
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-27 14:41:37 +0200

    Create animals functionality

--- R/get_animals.R
+++ R/get_animals.R
@@ -1,16 +1,43 @@
 # vliz.animals_view
 
 get_animals <- function(connection,
-                        netwerk_project = NULL,
+                        network_project = NULL,
                         animal_project = NULL,
                         species = NULL) {
-  NULL
 
-  # mainquery <- "
-  #   SELECT * from vliz.animals_view  # or animals table?
-  #   WHERE datetime  >= ? and datetime <= ?
-  #   "
-  # data <- data.frame(start_date, end_date)
-  # sqlExecute(connection, mainquery, data, fetch = TRUE)
+  check_connection(connection)
+
+  valid_networks <- get_projects(connection, project_type = "network") %>%
+    pull(projectcode)
+  check_null_or_value(network_project, valid_networks, "network_project")
+
+  valid_animals <- get_projects(connection, project_type = "animal") %>%
+    pull(projectcode)
+  check_null_or_value(animal_project, valid_animals, "animal_project")
+
+  if (is.null(network_project)) {
+    network_project = valid_networks
+  }
+  if (is.null(animal_project)) {
+    animal_project = valid_animals
+  }
+
+  # see https://github.com/inbo/etn/issues/18
+  projects <- data.frame(projectcode = c(network_project, animal_project),
+                         stringsAsFactors = FALSE)
+  project_names <- get_projects(connection) %>%
+    select(projectcode, name) %>%
+    right_join(projects, by = "projectcode") %>%
+    pull(name)
+
+  animals_query <- glue_sql(
+    "SELECT * FROM vliz.animals_view
+    WHERE projectname IN ({projects*})",
+    projects = project_names,
+    .con = connection
+  )
+
+  animals <- dbGetQuery(connection, animals_query)
+  animals
 
 }
