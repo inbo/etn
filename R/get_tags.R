fedb944d65520153187d79156c956a2e36984a44
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-27 16:57:58 +0200

    Create tags query functionality

--- R/get_tags.R
+++ R/get_tags.R
@@ -1,6 +1,27 @@
-# tags  (no view?)
-
 get_tags <- function(connection,
                      animal_project = NULL) {
-  NULL
+
+  check_connection(connection)
+
+  # valid inputs on animal projects
+  valid_animals_projects <-
+    get_projects(connection, project_type = "animal") %>%
+    pull(projectcode)
+  check_null_or_value(animal_project,  valid_animals_projects, "animal_project")
+  if (is.null(animal_project)) {
+    animal_project = valid_animals_projects
+  }
+
+  tags_query <- glue_sql("
+      SELECT tags.*, animals.projectcode
+      FROM vliz.tags
+        LEFT JOIN vliz.animal_tag_release ON (animal_tag_release.tag_fk = tags.id_pk)
+        LEFT JOIN vliz.animals_view animals ON (animals.id_pk = animal_tag_release.animal_fk)
+      WHERE projectcode IN ({project*})",
+    project = animal_project,
+    .con = connection
+  )
+  tags <- dbGetQuery(connection, tags_query)
+  tags
+
 }
