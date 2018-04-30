ab41b93e6224b184577377a18940b541e09e10af
damianooldoni <damiano.oldoni@inbo.be>
2018-04-30 19:23:32 +0200

    Add test get_projects

---
+++ tests/testthat/test_get_projects.R
@@ -0,0 +1,19 @@
+context("check_get_projects")
+
+# Valid connection
+con <- connect_to_etn(username, password)
+
+testthat::test_that("check_input_connection", {
+  expect_error(get_projects("I am not a connection"),
+               "Not a connection object to database.")
+  expect_error(get_projects(con, project_type = "bad_project_type"),
+               paste("Not valid input value(s) for project_type input",
+                     "argument.\nValid inputs are: animal and network."),
+               fixed = TRUE)
+})
+
+testthat::test_that("check_output_connection", {
+  prjcts <- get_projects(con)
+  expect_is(prjcts, "data.frame")
+  prjcts_animal <- get_projects(con, project_type = "animal")
+})
