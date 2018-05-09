55586ef85c9c0a35ac9516cfee1ff092e4a32f28
damianooldoni <damiano.oldoni@inbo.be>
2018-05-09 14:45:25 +0200

    Resolved merge conflict tex_chcke_connection

--- tests/testthat/test_check_connection.R
+++ tests/testthat/test_check_connection.R
@@ -7,4 +7,5 @@ con <- connect_to_etn(username, password)
 testthat::test_that("check_connection", {
   expect_error(check_connection("I am not a connection"))
   expect_true(check_connection(con))
+  expect_true(isClass(con, "PostgreSQL"))
 })
