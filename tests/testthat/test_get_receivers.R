224bb7b46858e9eea4506b0d3a88e7f237e1ea18
damianooldoni <damiano.oldoni@inbo.be>
2018-05-16 16:20:38 +0200

    Add content of test_get_receivers

--- tests/testthat/test_get_receivers.R
+++ tests/testthat/test_get_receivers.R
@@ -6,6 +6,17 @@ con <- connect_to_etn(
   password = Sys.getenv("pwd")
 )
 
+test1 <- get_receivers(con)
+test2 <- get_receivers(con, network_project = "thornton")
+test3 <- get_receivers(con, network_project = c("thornton", "leopold"))
+
 testthat::test_that("check_input_get_receivers", {
   expect_error(get_receivers(con, network_project = "Bad_network_project"))
+  expect_is(test1, "data.frame")
+  expect_is(test2, "data.frame")
+  expect_is(test3, "data.frame")
+  expect_gte(nrow(test1), nrow(test2))
+  expect_gte(nrow(test1), nrow(test3))
+  expect_gte(nrow(test3), nrow(test2))
 })
+
