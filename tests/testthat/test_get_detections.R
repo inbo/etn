d7695abb32b07d84c9e7921c479cc672d5175b06
damianooldoni <damiano.oldoni@inbo.be>
2018-05-18 17:22:38 +0200

    Add tests for get_detections

---
+++ tests/testthat/test_get_detections.R
@@ -0,0 +1,99 @@
+context("test_get_detections")
+
+# Valid connection
+con <- connect_to_etn(
+  username = Sys.getenv("userid"),
+  password = Sys.getenv("pwd")
+)
+
+testthat::test_that("test_input_get_etections", {
+  expect_error(get_detections("I am not a connection"),
+               "Not a connection object to database.")
+  expect_error(get_detections(con, network_project = "very_bad_project"))
+  expect_error(get_detections(con, network_project = c("thornton",
+                                                      "very_bad_project")))
+  expect_error(get_detections(con, animal_project = "very_bad_project",
+                              network_project = "thornton"))
+  expect_error(get_detections(con,
+                              animal_project = c("phd_reubens", "i_am_bad"),
+                              network_project = "thornton"))
+  expect_error(get_detections(con,
+                              animal_project = "phd_reubens",
+                              network_project = "thornton",
+                              start_date = "bad_date"))
+  expect_error(get_detections(con,
+                              animal_project = "phd_reubens",
+                              network_project = "thornton",
+                              start_date = "2011", end_date = "bad_brrrr"))
+  expect_error(get_detections(con,
+                              animal_project = "phd_reubens",
+                              network_project = "thornton",
+                              start_date = "2011", end_date = "bad_brrrr"))
+  expect_error(get_detections(con,
+                              animal_project = "phd_reubens",
+                              network_project = "thornton",
+                              deployment_station_name = "no_way"))
+  expect_error(get_detections(con,
+                              animal_project = "phd_reubens",
+                              network_project = "thornton",
+                              deployment_station_name = c("R03","no_way")))
+})
+
+
+start_date <- "2011"
+end_date <- "2011-02-01"
+start <- check_datetime(start_date, "start_date")
+end <- check_datetime(end_date, "end_date")
+test1 <- get_detections(con, start_date = start_date, limit = 5)
+test2 <- get_detections(con, start_date = start_date, limit = 2)
+animal_project <- "phd_reubens"
+network_project <- "thornton"
+limit <- 5
+test3<- get_detections(con, animal_project = animal_project,
+                       network_project = network_project, start_date = start_date,
+                       end_date = end_date, limit = 5)
+deployment_station_name <- "R03"
+test4 <- get_detections(con, animal_project = animal_project,
+                        network_project = network_project,
+                        deployment_station_name = deployment_station_name,
+                        limit = 5)
+transmitter <- "A69-1601-28281"
+test5 <- get_detections(con, transmitter = transmitter, limit = 5)
+testthat::test_that("test_output_get_detections", {
+  expect_equal(nrow(test1), 5)
+  expect_equal(nrow(test2), 2)
+  expect_equal(ncol(test1), ncol(test2))
+  expect_equal(ncol(test2), ncol(test3))
+  expect_equal(ncol(test3), ncol(test4))
+  expect_equal(ncol(test4), ncol(test5))
+  expect_true(test1 %>%
+                 select(datetime) %>%
+                 summarize(min_datetime = min(datetime)) %>%
+                 pull(min_datetime) > start)
+  expect_true(test2 %>%
+                select(datetime) %>%
+                summarize(min_datetime = min(datetime)) %>%
+                pull(min_datetime) > start)
+  expect_true(test3 %>%
+                select(datetime) %>%
+                summarize(min_datetime = min(datetime)) %>%
+                pull(min_datetime) > start)
+  expect_true(test3 %>%
+                select(datetime) %>%
+                summarize(max_datetime = max(datetime)) %>%
+                pull(max_datetime) < end)
+  expect_true(test3 %>%
+                distinct(animal_project_code) %>%
+                pull() == animal_project)
+  expect_true(test3 %>%
+                distinct(network_project_code) %>%
+                pull() == network_project)
+  expect_lte(test3 %>% nrow(), limit)
+  expect_true(test4 %>%
+                distinct(deployment_station_name) %>%
+                pull() == deployment_station_name)
+  expect_true(test5 %>%
+                distinct(transmitter) %>%
+                pull() == transmitter)
+})
+
