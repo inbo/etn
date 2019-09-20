56a1e8687c9ea45a3b0fd9a7759cb55fb90b25d6
damianooldoni <damiano.oldoni@inbo.be>
2019-09-20 10:56:36 +0200

    Pull tests from master

--- tests/testthat/test_get_animals.R
+++ tests/testthat/test_get_animals.R
@@ -6,18 +6,91 @@ con <- connect_to_etn(
   password = Sys.getenv("pwd")
 )
 
+# Valid column names
+valid_col_names_animals <- c(
+"projectname",
+"projectmember",
+"tag_code_space",
+"person_id",
+"animal_id",
+"scientific_name",
+"common_name",
+"length",
+"length_type",
+"length_units",
+"length2",
+"length2_type",
+"length2_units",
+"weight_units",
+"age",
+"age_units",
+"sex",
+"life_stage",
+"capture_location",
+"capture_depth",
+"utc_release_date_time",
+"comments",
+"est_tag_life","wild_or_hatchery",
+"stock",
+"dna_sample_taken",
+"treatment_type",
+"dissolved_oxygen",
+"sedative",
+"sedative_concentration",
+"temperature_change",
+"holding_temperature",
+"preop_holding_period",
+"post_op_holding_period",
+"surgery_location",
+"date_of_surgery",
+"anaesthetic","buffer",
+"anaesthetic_concentration",
+"buffer_concentration_in_anaesthetic",
+"anesthetic_concentration_in_recirculation",
+"buffer_concentration_in_recirculation",
+"id_pk",
+"catched_date_time",
+"capture_latitude",
+"capture_longitude",
+"release_latitude",
+"release_longitude",
+"surgery_latitude",
+"surgery_longitude",
+"recapture_date",
+"implant_type",
+"implant_method",
+"date_modified",
+"date_created",
+"release_location",
+"length3",
+"length3_type",
+"length3_units",
+"length4",
+"length4_type",
+"length4_units",
+"weight",
+"end_date_tag",
+"capture_method",
+"project_fk",
+"animal_tag_release_id_pk",
+"thelma_converted_code",
+"projectcode",
+"tag_full_id"
+)
+
 test1 <- get_animals(con, network_project = NULL)
-test2 <- get_animals(con, network_project = "thornton")
+test2 <- get_animals(con, animal_project = "phd_reubens")
 test3 <- get_animals(con, animal_project = "2012_leopoldkanaal")
 
-projects_test4 <- c("2012_leopoldkanaal", "phd_reubens")
+projects_test4 <- c("phd_reubens", "2012_leopoldkanaal")
 test4 <- get_animals(con, animal_project = projects_test4)
-animals_test4 <- c("Gadus morhua", "Sentinel", "Anguilla anguilla")
-test5 <- get_animals(con, scientific_name = animals_test4)
+animals_test5 <- c("Gadus morhua", "Sentinel", "Anguilla anguilla")
+test5 <- get_animals(con, scientific_name = animals_test5)
 animals_test6 <- c("Gadus morhua")
 projects_test6 <- "phd_reubens"
-test6 <- get_animals(con, animal_project = "phd_reubens",
+test6 <- get_animals(con, animal_project = projects_test6,
                      scientific_name = animals_test6)
+test7 <- get_animals(con, network_project = "thornton")
 
 testthat::test_that("test_input_get_animals", {
   expect_error(get_animals("I am not a connection"),
@@ -26,31 +99,67 @@ testthat::test_that("test_input_get_animals", {
 
 
 testthat::test_that("test_output_get_animals", {
+  library(dplyr)
   expect_is(test1, "data.frame")
   expect_is(test2, "data.frame")
+  expect_is(test3, "data.frame")
+  expect_is(test4, "data.frame")
+  expect_is(test5, "data.frame")
+  expect_is(test6, "data.frame")
+  expect_is(test7, "data.frame")
+  expect_true(all(names(test1) %in% valid_col_names_animals))
+  expect_true(all(valid_col_names_animals %in% names(test1)))
   expect_gte(nrow(test1), nrow(test2))
   expect_gte(nrow(test1), nrow(test3))
   expect_gte(nrow(test1), nrow(test4))
-  expect_gte(nrow(test4), nrow(test3))
   expect_gte(nrow(test1), nrow(test5))
-  expect_gte(nrow(test5), nrow(test6))
-  expect_equal(ncol(test1), ncol(test2))
-  expect_equal(ncol(test2), ncol(test3))
-  expect_equal(ncol(test3), ncol(test4))
-  expect_equal(ncol(test5), ncol(test6))
-  expect_identical(animals_test4,
-                   test4 %>%
-                     dplyr::distinct(scientific_name) %>%
-                     dplyr::pull(scientific_name))
+  expect_gte(nrow(test1), nrow(test6))
+  expect_gte(nrow(test1), nrow(test7))
+  expect_equal(names(test1), names(test2))
+  expect_equal(names(test1), names(test3))
+  expect_equal(names(test1), names(test4))
+  expect_equal(names(test1), names(test5))
+  expect_equal(names(test1), names(test6))
+  expect_equal(names(test1), names(test7))
+  expect_gte(test1 %>%
+               distinct(scientific_name) %>%
+               pull() %>%
+               length(),
+             test4 %>%
+               distinct(scientific_name) %>%
+               pull() %>%
+               length()
+  )
+  expect_lte(test4 %>%
+                     distinct(scientific_name) %>%
+                     pull() %>%
+                     length(),
+                   (test2 %>%
+                      distinct(scientific_name) %>%
+                      pull() %>%
+                      length() +
+                    test3 %>%
+                      distinct(scientific_name) %>%
+                      pull() %>%
+                      length())
+  )
   expect_true(all(projects_test4 %in% (test5 %>%
-                                         dplyr::distinct(projectcode) %>%
-                                         dplyr::pull(projectcode))))
+                                         distinct(projectcode) %>%
+                                         pull()))
+  )
   expect_identical(test6 %>%
-                     dplyr::distinct(scientific_name) %>%
-                     dplyr::pull(scientific_name),
-                   animals_test6)
+                     distinct(scientific_name) %>%
+                     pull(scientific_name),
+                   animals_test6
+  )
   expect_identical(test6 %>%
-                     dplyr::distinct(projectcode) %>%
-                     dplyr::pull(projectcode),
-                   projects_test6)
+                     distinct(projectcode) %>%
+                     pull(projectcode),
+                   projects_test6
+  )
+  expect_true(all(projects_test4 %in%
+                    (test7 %>%
+                       distinct(projectcode) %>%
+                       pull()))
+  )
 })
