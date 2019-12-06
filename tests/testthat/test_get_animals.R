44748ebb05cb90b93213385a9d750e7223de7d7a
damianooldoni <damiano.oldoni@inbo.be>
2019-12-06 17:02:14 +0100

    Remove network_project argument from tests get_animals + update col names returned
    See #75

--- tests/testthat/test_get_animals.R
+++ tests/testthat/test_get_animals.R
@@ -8,81 +8,93 @@ con <- connect_to_etn(
 
 # Valid column names
 valid_col_names_animals <- c(
-"projectname",
-"projectmember",
-"tag_code_space",
-"person_id",
-"animal_id",
-"scientific_name",
-"common_name",
-"length",
-"length_type",
-"length_units",
-"length2",
-"length2_type",
-"length2_units",
-"weight_units",
-"age",
-"age_units",
-"sex",
-"life_stage",
-"capture_location",
-"capture_depth",
-"utc_release_date_time",
-"comments",
-"est_tag_life","wild_or_hatchery",
-"stock",
-"dna_sample_taken",
-"treatment_type",
-"dissolved_oxygen",
-"sedative",
-"sedative_concentration",
-"temperature_change",
-"holding_temperature",
-"preop_holding_period",
-"post_op_holding_period",
-"surgery_location",
-"date_of_surgery",
-"anaesthetic","buffer",
-"anaesthetic_concentration",
-"buffer_concentration_in_anaesthetic",
-"anesthetic_concentration_in_recirculation",
-"buffer_concentration_in_recirculation",
-"id_pk",
-"catched_date_time",
-"capture_latitude",
-"capture_longitude",
-"release_latitude",
-"release_longitude",
-"surgery_latitude",
-"surgery_longitude",
-"recapture_date",
-"implant_type",
-"implant_method",
-"date_modified",
-"date_created",
-"release_location",
-"length3",
-"length3_type",
-"length3_units",
-"length4",
-"length4_type",
-"length4_units",
-"weight",
-"end_date_tag",
-"capture_method",
-"project_fk",
-"animal_tag_release_id_pk",
-"thelma_converted_code",
-"projectcode",
-"tag_full_id"
+  "projectname",
+  "projectmember",
+  "projectcode",
+  "principal_investigator",
+  "owner_organization",
+  "tag_code_space",
+  "id_code",
+  "tag_full_id",
+  "thelma_converted_code",
+  "type",
+  "model",
+  "serial_number",
+  "activation_date",
+  "name",
+  "manufacturer",
+  "person_id",
+  "animal_id",
+  "scientific_name",
+  "common_name",
+  "length",
+  "length_type",
+  "length_units",
+  "length2",
+  "length2_type",
+  "length2_units",
+  "weight_units",
+  "age",
+  "age_units",
+  "sex",
+  "life_stage",
+  "capture_location",
+  "utc_release_date_time",
+  "comments",
+  "est_tag_life",
+  "wild_or_hatchery",
+  "stock",
+  "dna_sample_taken",
+  "treatment_type",
+  "dissolved_oxygen",
+  "sedative",
+  "sedative_concentration",
+  "temperature_change",
+  "holding_temperature",
+  "preop_holding_period",
+  "post_op_holding_period",
+  "surgery_location",
+  "date_of_surgery",
+  "anaesthetic",
+  "buffer",
+  "anaesthetic_concentration",
+  "buffer_concentration_in_anaesthetic",
+  "anesthetic_concentration_in_recirculation",
+  "buffer_concentration_in_recirculation",
+  "id_pk",
+  "catched_date_time",
+  "capture_latitude",
+  "capture_longitude",
+  "release_latitude",
+  "release_longitude",
+  "surgery_latitude",
+  "surgery_longitude",
+  "recapture_date",
+  "capture_depth",
+  "implant_type",
+  "implant_method",
+  "date_modified",
+  "date_created",
+  "release_location",
+  "length3",
+  "length3_type",
+  "length4_type",
+  "length4_units",
+  "length3_units",
+  "length4",
+  "weight",
+  "end_date_tag",
+  "capture_method",
+  "project_fk",
+  "animal_tag_release_id_pk",
+  "aphia_id"
 )
 
 test1 <- get_animals(con)
 test2 <- get_animals(con, animal_project = "phd_reubens")
-test3 <- get_animals(con, animal_project = "2012_leopoldkanaal")
+test3 <- get_animals(con, animal_project = "2013_albertkanaal")
 
-projects_test4 <- c("phd_reubens", "2012_leopoldkanaal")
+projects_test4 <- c("phd_reubens", "2013_albertkanaal")
 test4 <- get_animals(con, animal_project = projects_test4)
 animals_test5 <- c("Gadus morhua", "Sentinel", "Anguilla anguilla")
 test5 <- get_animals(con, scientific_name = animals_test5)
@@ -90,7 +102,6 @@ animals_test6 <- c("Gadus morhua")
 projects_test6 <- "phd_reubens"
 test6 <- get_animals(con, animal_project = projects_test6,
                      scientific_name = animals_test6)
-test7 <- get_animals(con, network_project = "thornton")
 
 testthat::test_that("test_input_get_animals", {
   expect_error(get_animals("I am not a connection"),
@@ -106,7 +117,6 @@ testthat::test_that("test_output_get_animals", {
   expect_is(test4, "data.frame")
   expect_is(test5, "data.frame")
   expect_is(test6, "data.frame")
-  expect_is(test7, "data.frame")
   expect_true(all(names(test1) %in% valid_col_names_animals))
   expect_true(all(valid_col_names_animals %in% names(test1)))
   expect_gte(nrow(test1), nrow(test2))
@@ -114,13 +124,12 @@ testthat::test_that("test_output_get_animals", {
   expect_gte(nrow(test1), nrow(test4))
   expect_gte(nrow(test1), nrow(test5))
   expect_gte(nrow(test1), nrow(test6))
-  expect_gte(nrow(test1), nrow(test7))
+  expect_equal(nrow(test3), 309)
   expect_equal(names(test1), names(test2))
   expect_equal(names(test1), names(test3))
   expect_equal(names(test1), names(test4))
   expect_equal(names(test1), names(test5))
   expect_equal(names(test1), names(test6))
-  expect_equal(names(test1), names(test7))
   expect_gte(test1 %>%
                distinct(scientific_name) %>%
                pull() %>%
@@ -156,10 +165,5 @@ testthat::test_that("test_output_get_animals", {
                      distinct(projectcode) %>%
                      pull(projectcode),
                    projects_test6
-  )
-  expect_true(all(projects_test4 %in%
-                    (test7 %>%
-                       distinct(projectcode) %>%
-                       pull()))
   )
 })
