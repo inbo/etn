b101dc281239ccef35b102f7fe7a8ebf2c2b2841
peterdesmet <peter.desmet@inbo.be>
2020-02-19 15:29:04 +0100

    Make use of new views for receivers, expecting unique receivers
    Drop the code to select distinct receivers, as that is what is expected by the new view.
    Add new test to test on unique receiver pk and id

--- R/get_receivers.R
+++ R/get_receivers.R
@@ -1,6 +1,6 @@
-#' Get receiver data
+#' Get receiver metadata
 #'
-#' Get all or specific, filtered on network project, receiver data.
+#' Get metadata for receivers, with option to filter on network project.
 #'
 #' @param connection A valid connection with the ETN database.
 #' @param network_project (string) One or more network projects.
@@ -19,20 +19,18 @@
 #' \dontrun{
 #' con <- connect_to_etn(your_username, your_password)
 #'
-#' # Get all receivers data
-#' get_receivers(con)
+#' # Get metadata for all receivers
+#' receivers <- get_receivers(con)
 #'
-#' # Get receivers data filtered on a single network project
-#' get_receivers(con, network_project = "thornton")
-#'
-#' # Get receivers data filtered on a multiple network projects
-#' get_receivers(con, network_project = c("thornton", "2012_leopoldkanaal"))
+#' # Get metadata for receivers linked to specific network project(s)
+#' receivers <- get_receivers(con, network_project = "thornton")
+#' receivers <- get_receivers(con, network_project = c("thornton", "2012_leopoldkanaal"))
 #' }
 get_receivers <- function(connection,
                           network_project = NULL) {
   check_connection(connection)
 
-  # valid inputs on animal projects
+  # Valid inputs on network projects
   valid_network_projects <-
     get_projects(connection, project_type = "network") %>%
     pull(.data$projectcode)
@@ -43,28 +41,22 @@ get_receivers <- function(connection,
   }
 
   receivers_query <- glue_sql("
-      SELECT DISTINCT receivers.* ,
-        deployments.projectcode,
-        etn_group.name as owner_organisation
-      FROM vliz.receivers
-        JOIN vliz.deployments_view deployments ON (receivers.id_pk = deployments.receiver_fk)
-        JOIN vliz.etn_group AS etn_group ON (receivers.owner_group_fk = etn_group.id_pk)
-      WHERE projectcode IN ({project*})",
-                              project = network_project,
-                              .con = connection
+    SELECT *
+    FROM vliz.receivers_view2
+    WHERE network_project_code IN ({network_project*})
+    ", .con = connection
   )
   receivers <- dbGetQuery(connection, receivers_query)
 
   # we still have multiple records of receivers, as project codes are coupled to
   # deployments and a receiver can have multiple deployments aka projects.
   # combine the individual network projects in a single row:
-  receivers %>%
-    group_by(.data$id_pk) %>%
-    mutate(projectcode = paste(.data$projectcode, collapse = ",")) %>%
-    rename(network_projectcode = .data$projectcode) %>%
-    ungroup() %>%
-    distinct()
+  # receivers %>%
+  #   group_by(.data$id_pk) %>%
+  #   mutate(projectcode = paste(.data$projectcode, collapse = ",")) %>%
+  #   rename(network_projectcode = .data$projectcode) %>%
+  #   ungroup() %>%
+  #   distinct()
 
   as_tibble(receivers)
-
 }
--- tests/testthat/test-get_receivers.R
+++ tests/testthat/test-get_receivers.R
@@ -4,35 +4,35 @@ con <- connect_to_etn(
   password = Sys.getenv("pwd")
 )
 
-# valid column names
+# Valid column names
 valid_col_names_receivers <- c(
-    "serial_number",
-    "model_number",
-    "modem_address",
-    "expected_battery_life",
-    "ar_model_number",
-    "ar_serial_number",
-    "ar_expected_battery_life",
-    "ar_voltage_at_deploy",
-    "ar_tilt_after_deploy",
-    "ar_interrogate_code",
-    "ar_receive_frequency",
-    "ar_reply_frequency",
-    "ar_ping_rate",
-    "ar_enable_code_address",
-    "ar_release_code",
-    "ar_disable_code",
-    "ar_tilt_code",
-    "id_pk",
-    "receiver",
-    "built_in_tag_fk",
-    "status",
-    "receiver_type",
-    "manufacturer_fk",
-    "owner_group_fk",
-    "financing_project_fk",
-    "projectcode",
-    "owner_organisation"
+  "pk",
+  "receiver_id",
+  "network_project_code",
+  "application_type",
+  "telemetry_type",
+  "manufacturer",
+  "receiver_id_model",
+  "receiver_id_serial_number",
+  "modem_address",
+  "status",
+  "battery_estimated_life",
+  "owner_organization",
+  "financing_project",
+  "built_in_tag_id",
+  "ar_model_number",
+  "ar_serial_number",
+  "ar_battery_estimated_life",
+  "ar_voltage_at_deploy",
+  "ar_interrogate_code",
+  "ar_receive_frequency",
+  "ar_reply_frequency",
+  "ar_ping_rate",
+  "ar_enable_code_address",
+  "ar_release_code",
+  "ar_disable_code",
+  "ar_tilt_code",
+  "ar_tilt_after_deploy"
 )
 
 test1 <- get_receivers(con)
@@ -56,7 +56,8 @@ testthat::test_that("test_output_get_receivers", {
   expect_gt(nrow(test1), nrow(test2))
   expect_gte(nrow(test1), nrow(test3))
   expect_gte(nrow(test3), nrow(test2))
+  expect_equal(nrow(test1), nrow(test1 %>% distinct(pk)))
+  expect_equal(nrow(test1), nrow(test1 %>% distinct(receiver_id)))
   expect_equal(names(test1), names(test2))
   expect_equal(names(test1), names(test3))
 })
-
