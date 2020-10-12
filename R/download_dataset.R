3cfb3d43716e7f2fd435a4cb293b228690d4c0c6
peterdesmet <peter.desmet@inbo.be>
2020-10-12 18:35:38 +0200

    Use messages for progress + stats, warning for odd data

--- R/download_dataset.R
+++ R/download_dataset.R
@@ -45,32 +45,47 @@ download_dataset <- function(connection = con,
   }
 
   # Start downloading
-  message(glue("Downloading data to directory \"{directory}\" ..."))
+  message(glue("Downloading data to directory \"{directory}\":"))
 
-  # Animals: select on animal_project_code and scientific_name
+  # ANIMALS
+  message("* (1/6): downloading animals.csv")
+  # Select on animal_project_code and scientific_name
   animals <- get_animals(
     connection = con,
     animal_project_code = animal_project_code,
     scientific_name = scientific_name
   )
+  write_csv(animals, paste(directory, "animals.csv", sep="/"), na = "")
 
-  # Tags: select on tags associated with animals
+  # TAGS
+  message("* (2/6): downloading tags.csv")
+  # Select on tags associated with animals
   tag_ids <-
     animals %>%
     distinct(tag_id) %>%
     pull()
-  tags <- get_tags(connection = con, tag_id = tag_ids, include_ref_tags = TRUE)
+  tags <- get_tags(
+    connection = con,
+    tag_id = tag_ids,
+    include_ref_tags = TRUE # TODO: should this be the default?
+  )
+  write_csv(tags, paste(directory, "tags.csv", sep="/"), na = "")
 
-  # Detections: select on animal_project_code and scientific_name
+  # DETECTIONS
+  message("* (3/6): downloading detections.csv")
+  # Select on animal_project_code and scientific_name
   detections <- get_detections(
     connection = con,
     animal_project_code = animal_project_code,
     scientific_name = scientific_name,
     limit = FALSE
   )
+  write_csv(detections, paste(directory, "detections.csv", sep="/"), na = "")
 
-  # Deployments: select on network_project_codes found in detections to get all
-  # deployments (including those without detections for animal_project_code)
+  # DEPLOYMENTS
+  message("* (4/6): downloading deployments.csv")
+  # Select on network_project_codes found in detections to get all deployments,
+  # including those without detections for animal_project_code
   network_project_codes <-
     detections %>%
     distinct(network_project_code) %>%
@@ -85,22 +100,23 @@ download_dataset <- function(connection = con,
   deployments <-
     deployments %>%
     mutate(comments = str_replace_all(comments, "[\r\n]+", " "))
+  write_csv(deployments, paste(directory, "deployments.csv", sep="/"), na = "")
 
-  # Receivers: select on receivers associated with deployments
+  # RECEIVERS
+  message("* (5/6): downloading receivers.csv")
+  # Select on receivers associated with deployments
   receiver_ids <-
     deployments %>%
     distinct(receiver_id) %>%
     pull()
-  receivers <- get_receivers(con, receiver_id = receiver_ids)
-
-  # Write data to file
-  write_csv(animals, paste(directory, "animals.csv", sep="/"), na = "")
-  write_csv(tags, paste(directory, "tags.csv", sep="/"), na = "")
-  write_csv(detections, paste(directory, "detections.csv", sep="/"), na = "")
-  write_csv(deployments, paste(directory, "deployments.csv", sep="/"), na = "")
+  receivers <- get_receivers(
+    con,
+    receiver_id = receiver_ids
+  )
   write_csv(receivers, paste(directory, "receivers.csv", sep="/"), na = "")
 
-  # Add datapackage.json
+  # DATAPACKAGE.JSON
+  message("* (6/6): adding datapackage.json as file metadata")
   datapackage <- system.file("assets", "datapackage.json", package = "etn")
   file.copy(datapackage, paste(directory, "datapackage.json", sep = "/"), overwrite = TRUE)
 
@@ -111,6 +127,18 @@ download_dataset <- function(connection = con,
     arrange(scientific_name) %>%
     pull()
 
+  message("")
+  message(glue("\nSummary statistics for dataset \"{animal_project_code}\":"))
+  message("* number of animals:           ", nrow(animals))
+  message("* number of tags:              ", nrow(tags))
+  message("* number of detections:        ", nrow(detections))
+  message("* number of deployments:       ", nrow(deployments))
+  message("* number of receivers:         ", nrow(receivers))
+  message("* included scientific names:   ", paste(scientific_names, collapse = ", "))
+  message("* included network projects:   ", paste(network_project_codes, collapse = ", "))
+  message("")
+
+  # Create warnings
   animals_multiple_tags <-
     animals %>%
     filter(str_detect(tag_id, ",")) %>%
@@ -130,34 +158,13 @@ download_dataset <- function(connection = con,
     distinct(deployment_fk) %>%
     pull()
 
-  stats <- tibble(
-    "stat" = c(
-      "animal project",
-      "animals",
-      "tags",
-      "detections",
-      "deployments",
-      "receivers",
-      "scientific names",
-      "network projects",
-      "animals with multiple tags",
-      "tags with multiple animals",
-      "deployments without network"
-    ),
-    "value" = c(
-      animal_project_code,
-      nrow(animals),
-      nrow(tags),
-      nrow(detections),
-      nrow(deployments),
-      nrow(receivers),
-      paste(scientific_names, collapse = ", "),
-      paste(network_project_codes, collapse = ", "),
-      paste(animals_multiple_tags, collapse = ", "),
-      paste(tags_multiple_animals, collapse = ", "),
-      paste(orphaned_deployments, collapse = ", ")
-    )
-  )
-
-  return(stats)
+  if(length(animals_multiple_tags) > 0) {
+    warning("Animals with multiple tags: ", paste(animals_multiple_tags, collapse = ", "))
+  }
+  if(length(tags_multiple_animals) > 0) {
+    warning("Tags associated with multiple animals: ", paste(tags_multiple_animals, collapse = ", "))
+  }
+  if(length(orphaned_deployments) > 0) {
+    warning("Deployments without network project: ", paste(orphaned_deployments, collapse = ", "))
+  }
 }
