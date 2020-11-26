4f6ec1e5f12b09a424ceb34d90a242e57f1556b6
peterdesmet <peter.desmet@inbo.be>
2020-11-26 13:32:15 +0100

    Drop AS col_name

--- R/list_network_project_codes.R
+++ R/list_network_project_codes.R
@@ -11,7 +11,7 @@
 #'   present in `projects_view2`.
 list_network_project_codes <- function(connection = con) {
   query <- glue_sql(
-    "SELECT DISTINCT project_code AS project_code FROM vliz.projects_view2 WHERE project_type = 'network'",
+    "SELECT DISTINCT project_code FROM vliz.projects_view2 WHERE project_type = 'network'",
     .con = connection
   )
   data <- dbGetQuery(connection, query)
