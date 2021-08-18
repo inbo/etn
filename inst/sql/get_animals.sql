1e32c0612859d405e0a19f1b753ae62bf15b768f
peterdesmet <peter.desmet@inbo.be>
2021-08-18 18:56:37 +0200

    Prefix fields with animal.

--- inst/sql/get_animals.sql
+++ inst/sql/get_animals.sql
@@ -4,129 +4,129 @@ SELECT
 -- animal_id
   animal.id_pk AS ANIMAL_ID,
 -- animal_project_code
-  project.projectcode AS ANIMAL_PROJECT_CODE,
+  animal_project.projectcode AS ANIMAL_PROJECT_CODE,
 -- tag_id
   acoustic_tag.tag_full_id AS TAG_ID,
 -- tag_fk
-  tag.id_pk AS TAG_FK,
+  tag.id_pk AS TAG_FK, -- This fk can remain
 -- scientific_name
-  scientific_name AS SCIENTIFIC_NAME,
+  animal.scientific_name AS SCIENTIFIC_NAME,
 -- common_name
-  common_name AS COMMON_NAME,
+  animal.common_name AS COMMON_NAME,
 -- aphia_id
-  aphia_id AS APHIA_ID,
+  animal.aphia_id AS APHIA_ID,
 -- animal_label
-  animal_id AS ANIMAL_LABEL,
+  animal.animal_id AS ANIMAL_LABEL,
 -- animal_nickname
-  animal_nickname AS ANIMAL_NICKNAME,
+  animal.animal_nickname AS ANIMAL_NICKNAME,
 -- tagger
-  tagger AS TAGGER,
+  animal.tagger AS TAGGER,
 -- capture_date_time
-  catched_date_time AS CAPTURE_DATE_TIME,
+  animal.catched_date_time AS CAPTURE_DATE_TIME,
 -- capture_location
-  capture_location AS CAPTURE_LOCATION,
+  animal.capture_location AS CAPTURE_LOCATION,
 -- capture_latitude
-  capture_latitude AS CAPTURE_LATITUDE,
+  animal.capture_latitude AS CAPTURE_LATITUDE,
 -- capture_longitude
-  capture_longitude AS CAPTURE_LONGITUDE,
+  animal.capture_longitude AS CAPTURE_LONGITUDE,
 -- capture_method
-  capture_method AS CAPTURE_METHOD,
+  animal.capture_method AS CAPTURE_METHOD,
 -- capture_depth
-  capture_depth AS CAPTURE_DEPTH,
+  animal.capture_depth AS CAPTURE_DEPTH,
 -- capture_temperature_change
-  temperature_change AS CAPTURE_TEMPERATURE_CHANGE,
+  animal.temperature_change AS CAPTURE_TEMPERATURE_CHANGE,
 -- release_date_time
-  utc_release_date_time AS RELEASE_DATE_TIME,
+  animal.utc_release_date_time AS RELEASE_DATE_TIME,
 -- release_location
-  release_location AS RELEASE_LOCATION,
+  animal.release_location AS RELEASE_LOCATION,
 -- release_latitude
-  release_latitude AS RELEASE_LATITUDE,
+  animal.release_latitude AS RELEASE_LATITUDE,
 -- release_longitude
-  release_longitude AS RELEASE_LONGITUDE,
+  animal.release_longitude AS RELEASE_LONGITUDE,
 -- recapture_date_time
-  recapture_date AS RECAPTURE_DATE_TIME,
+  animal.recapture_date AS RECAPTURE_DATE_TIME,
 -- length1_type
-  length_type AS LENGTH1_TYPE,
+  animal.length_type AS LENGTH1_TYPE,
 -- length1
-  length AS LENGTH1,
+  animal.length AS LENGTH1,
 -- length1_unit
-  length_units AS LENGTH1_UNIT,
+  animal.length_units AS LENGTH1_UNIT,
 -- length2_type
-  length2_type AS LENGTH2_TYPE,
+  animal.length2_type AS LENGTH2_TYPE,
 -- length2
-  length2 AS LENGTH2,
+  animal.length2 AS LENGTH2,
 -- length2_unit
-  length2_units AS LENGTH2_UNIT,
+  animal.length2_units AS LENGTH2_UNIT,
 -- length3_type
-  length3_type AS LENGTH3_TYPE,
+  animal.length3_type AS LENGTH3_TYPE,
 -- length3
-  length3 AS LENGTH3,
+  animal.length3 AS LENGTH3,
 -- length3_unit
-  length3_units AS LENGTH3_UNIT,
+  animal.length3_units AS LENGTH3_UNIT,
 -- length4_type
-  length4_type AS LENGTH4_TYPE,
+  animal.length4_type AS LENGTH4_TYPE,
 -- length4
-  length4 AS LENGTH4,
+  animal.length4 AS LENGTH4,
 -- length4_unit
-  length4_units AS LENGTH4_UNIT,
+  animal.length4_units AS LENGTH4_UNIT,
 -- weight
-  weight AS WEIGHT,
+  animal.weight AS WEIGHT,
 -- weight_unit
-  weight_units AS WEIGHT_UNIT,
+  animal.weight_units AS WEIGHT_UNIT,
 -- age
-  age AS AGE,
+  animal.age AS AGE,
 -- age_unit
-  age_units AS AGE_UNIT,
+  animal.age_units AS AGE_UNIT,
 -- sex
-  sex AS SEX,
+  animal.sex AS SEX,
 -- life_stage
-  life_stage AS LIFE_STAGE,
+  animal.life_stage AS LIFE_STAGE,
 -- wild_or_hatchery
-  wild_or_hatchery AS WILD_OR_HATCHERY,
+  animal.wild_or_hatchery AS WILD_OR_HATCHERY,
 -- stock
-  stock AS STOCK,
+  animal.stock AS STOCK,
 -- surgery_date_time
-  date_of_surgery AS SURGERY_DATE_TIME,
+  animal.date_of_surgery AS SURGERY_DATE_TIME,
 -- surgery_location
-  surgery_Location AS SURGERY_LOCATION,
+  animal.surgery_Location AS SURGERY_LOCATION,
 -- surgery_latitude
-  surgery_latitude AS SURGERY_LATITUDE,
+  animal.surgery_latitude AS SURGERY_LATITUDE,
 -- surgery_longitude
-  surgery_longitude AS SURGERY_LONGITUDE,
+  animal.surgery_longitude AS SURGERY_LONGITUDE,
 -- treatment_type
-  treatment_type AS TREATMENT_TYPE,
+  animal.treatment_type AS TREATMENT_TYPE,
 -- tagging_type
-  implant_type AS TAGGING_TYPE,
+  animal.implant_type AS TAGGING_TYPE,
 -- tagging_methodology
-  implant_method AS TAGGING_METHODOLOGY,
+  animal.implant_method AS TAGGING_METHODOLOGY,
 -- dna_sample
-  dna_sample_taken AS DNA_SAMPLE,
+  animal.dna_sample_taken AS DNA_SAMPLE,
 -- sedative
-  sedative AS SEDATIVE,
+  animal.sedative AS SEDATIVE,
 -- sedative_concentration
-  sedative_concentration AS SEDATIVE_CONCENTRATION,
+  animal.sedative_concentration AS SEDATIVE_CONCENTRATION,
 -- anaesthetic
-  anaesthetic AS ANAESTHETIC,
+  animal.anaesthetic AS ANAESTHETIC,
 -- buffer
-  buffer AS BUFFER,
+  animal.buffer AS BUFFER,
 -- anaesthetic_concentration
-  anaesthetic_concentration AS ANAESTHETIC_CONCENTRATION,
+  animal.anaesthetic_concentration AS ANAESTHETIC_CONCENTRATION,
 -- buffer_concentration_in_anaesthetic
-  buffer_concentration_in_anaesthetic AS BUFFER_CONCENTRATION_IN_ANAESTHETIC,
+  animal.buffer_concentration_in_anaesthetic AS BUFFER_CONCENTRATION_IN_ANAESTHETIC,
 -- anaesthetic_concentration_in_recirculation
-  anesthetic_concentration_In_recirculation AS ANAESTHETIC_CONCENTRATION_IN_RECIRCULATION,
+  animal.anesthetic_concentration_In_recirculation AS ANAESTHETIC_CONCENTRATION_IN_RECIRCULATION,
 -- buffer_concentration_in_recirculation
-  buffer_concentration_in_recirculation AS BUFFER_CONCENTRATION_IN_RECIRCULATION,
+  animal.buffer_concentration_in_recirculation AS BUFFER_CONCENTRATION_IN_RECIRCULATION,
 -- dissolved_oxygen
-  dissolved_oxygen AS DISSOLVED_OXYGEN,
+  animal.dissolved_oxygen AS DISSOLVED_OXYGEN,
 -- pre_surgery_holding_period
-  preop_holding_period AS PRE_SURGERY_HOLDING_PERIOD,
+  animal.preop_holding_period AS PRE_SURGERY_HOLDING_PERIOD,
 -- post_surgery_holding_period
-  post_op_holding_period AS POST_SURGERY_HOLDING_PERIOD,
+  animal.post_op_holding_period AS POST_SURGERY_HOLDING_PERIOD,
 -- holding_temperature
-  holding_temperature AS HOLDING_TEMPERATURE,
+  animal.holding_temperature AS HOLDING_TEMPERATURE,
 -- comments
-  comments AS COMMENTS
+  animal.comments AS COMMENTS
 
 FROM common.animal_release AS animal
   LEFT JOIN common.animal_release_tag_device AS animal_with_tag
@@ -135,6 +135,5 @@ FROM common.animal_release AS animal
     ON animal_with_tag.tag_device_fk = tag.id_pk
   LEFT JOIN acoustic.tags AS acoustic_tag
     ON tag.id_pk = acoustic_tag.tag_device_fk
-
-  LEFT JOIN common.projects AS project
-    ON animal.project_fk = project.id
+  LEFT JOIN common.projects AS animal_project
+    ON animal.project_fk = animal_project.id
