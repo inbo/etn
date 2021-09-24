3e8897c8e766ae7e3332159a761bf0af7a54ae2b
peterdesmet <peter.desmet@inbo.be>
2021-09-24 18:24:28 +0200

    Update example for acoustic_tag_id

--- tests/testthat/test-list_acoustic_tag_ids.R
+++ tests/testthat/test-list_acoustic_tag_ids.R
@@ -6,5 +6,5 @@ con <- connect_to_etn(
 testthat::test_that("Test output", {
   expect_is(list_acoustic_tag_ids(con), "character")
   expect_false(any(duplicated(list_acoustic_tag_ids(con))))
-  expect_true("A69-1303-65302" %in% list_acoustic_tag_ids(con))
+  expect_true("A69-1601-28294" %in% list_acoustic_tag_ids(con))
 })
