e2a54a3f34cf8ccc02074b78b03a06b4a8406c6e
peterdesmet <peter.desmet@inbo.be>
2021-10-08 17:47:46 +0200

    Create list_cpod_project_codes()

---
+++ R/list_cpod_project_codes.R
@@ -0,0 +1,22 @@
+#' List all available cpod project codes
+#'
+#' @param connection A connection to the ETN database. Defaults to `con`.
+#'
+#' @export
+#'
+#' @importFrom glue glue_sql
+#' @importFrom DBI dbGetQuery
+#' @importFrom readr read_file
+#'
+#' @return A vector of all unique `project_code` of `type = "cpod"` in
+#'   `project.sql`.
+list_cpod_project_codes <- function(connection = con) {
+  project_query <- glue_sql(read_file(system.file("sql", "project.sql", package = "etn")), .con = connection)
+  query <- glue_sql(
+    "SELECT DISTINCT project_code FROM ({project_query}) AS project WHERE project_type = 'cpod'",
+    .con = connection
+  )
+  data <- dbGetQuery(connection, query)
+
+  sort(data$project_code)
+}
