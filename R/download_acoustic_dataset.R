db0c8a7e12f929d1d4e5e7cd6deac4a0d11613b9
peterdesmet <peter.desmet@inbo.be>
2021-10-11 20:27:56 +0200

    Create download_acoustic_dataset()
    Functional, but not tested

--- R/download_acoustic_dataset.R
+++ R/download_acoustic_dataset.R
@@ -1,8 +1,8 @@
-#' Download data package
+#' Download acoustic data package
 #'
-#' Download all data related to an **animal project** as a data package that can
-#' be deposited in a research data repository. Includes option to filter on
-#' scientific names.
+#' Download all acoustic data related to an **animal project** as a data
+#' package that can be deposited in a research data repository. Includes option
+#' to filter on scientific names.
 #'
 #' The data are downloaded as a
 #' **[Frictionless Data Package](https://frictionlessdata.io/data-package/)**
@@ -12,21 +12,21 @@
 #' --- | ---
 #' `animals.csv` | Animals related to an `animal_project_code`, optionally filtered on `scientific_name`(s), as returned by `get_animals()`.
 #' `tags.csv` | Tags associated with the selected animals, as returned by `get_tags()`.
-#' `detections.csv` | Detections for the selected animals, as returned by `get_detections()`.
-#' `deployments.csv` | Deployments for the `network_project_code`(s) found in detections, as returned by `get_deployments()`. This allows users to see when receivers were deployed, even if these did not detect the selected animals.
-#' `receivers.csv` | Receivers for the selected deployments, as returned by `get_receivers()`.
+#' `detections.csv` | Acoustic detections for the selected animals, as returned by `get_acoustic_detections()`.
+#' `deployments.csv` | Acoustic deployments for the `acoustic_project_code`(s) found in detections, as returned by `get_acoustic_deployments()`. This allows users to see when receivers were deployed, even if these did not detect the selected animals.
+#' `receivers.csv` | Acoustic receivers for the selected deployments, as returned by `get_acoustic_receivers()`.
 #' `datapackage.json` | A [Frictionless Table Schema](https://specs.frictionlessdata.io/table-schema/) metadata file describing the fields and relations of the above csv files. This file is copied from [here](https://github.com/inbo/etn/blob/master/inst/assets/datapackage.json) and can be used to validate the data package.
 #'
 #' The function will report the number of records per csv file, as well as the
-#' included scientific names and network projects. Warnings will be raised for:
+#' included scientific names and acoustic projects. Warnings will be raised for:
 #'
 #' - Animals with multiple tags
 #' - Tags associated with multiple animals
-#' - Deployments without network project: these deployments will not be listed
+#' - Deployments without acoustic project: these deployments will not be listed
 #'   in `deployments.csv` and will therefore raise a foreign key validation
 #'   error.
-#' - Duplicate detections: detections with the duplicate `pk`. These are
-#'   removed by the function in `detections.csv`.
+#' - Duplicate detections: detections with the duplicate `detection_id`. These
+#'   are removed by the function in `detections.csv`.
 #'
 #' **Important**: The data are downloaded _as is_ from the database, i.e. no
 #' quality or consistency checks are performed by this function. We therefore
@@ -39,8 +39,7 @@
 #' @param animal_project_code Character. Animal project you want to download
 #'   data for. Required.
 #' @param scientific_name Character (vector). One or more scientific names.
-#'   Defaults to no filter (i.e. include all scientific names, including
-#'   reference tags).
+#'   Defaults to no all (all scientific names, include "Sync tag", etc.).
 #' @param directory Character. Relative path to local download directory.
 #'   Defaults to creating a directory named after animal project code. Existing
 #'   files of the same name will be overwritten.
@@ -48,14 +47,14 @@
 #' @export
 #'
 #' @importFrom glue glue
-#' @importFrom dplyr %>% arrange distinct filter group_by mutate n pull select summarize
+#' @importFrom dplyr .data %>% arrange distinct filter group_by mutate n pull select summarize
 #' @importFrom readr write_csv
 #' @importFrom stringr str_detect str_replace_all
 #'
 #' @examples
 #' \dontrun{
 #' # Download data for the 2012_leopoldkanaal animal project (all scientific names)
-#' download_dataset_old(con, animal_project_code = "2012_leopoldkanaal")
+#' download_acoustic_dataset(con, animal_project_code = "2012_leopoldkanaal")
 #' #> Downloading data to directory "2012_leopoldkanaal":
 #' #> (existing files with the same name will be overwritten)
 #' #> * (1/6): downloading animals.csv
@@ -74,16 +73,16 @@
 #' #> * first date of detection:     2012-07-03
 #' #> * last date of detection:      2020-09-28
 #' #> * included scientific names:   Anguilla anguilla
-#' #> * included network projects:   albert, bpns, JJ_Belwind, leopold, pc4c, ws2, zeeschelde
+#' #> * included acoustic projects:   albert, bpns, JJ_Belwind, leopold, pc4c, ws2, zeeschelde
 
 #' #> Warning message:
-#' #> In download_dataset_old(con, animal_project_code = "2012_leopoldkanaal") :
+#' #> In download_acoustic_dataset(con, animal_project_code = "2012_leopoldkanaal") :
 #' #> Found tags associated with multiple animals: A69-1601-29925
 #' }
-download_dataset_old <- function(connection = con,
-                             animal_project_code,
-                             scientific_name = NULL,
-                             directory = animal_project_code) {
+download_acoustic_dataset <- function(connection = con,
+                                      animal_project_code,
+                                      scientific_name = NULL,
+                                      directory = animal_project_code) {
   # Check connection
   check_connection(connection)
 
@@ -91,14 +90,14 @@ download_dataset_old <- function(connection = con,
   if (missing(animal_project_code) || is.null(animal_project_code)) {
     stop("Please provide an animal_project_code")
   } else {
-    valid_animal_project_codes <- list_animal_project_codes(connection)
+    valid_animal_project_codes <- tolower(list_animal_project_codes(connection))
     check_value(animal_project_code, valid_animal_project_codes, "animal_project_code")
   }
 
   # Check scientific_name
   if (!is.null(scientific_name)) {
-    scientific_name_ids <- list_scientific_names(connection)
-    check_value(scientific_name, scientific_name_ids, "scientific_name")
+    valid_scientific_names <- list_scientific_names(connection)
+    check_value(scientific_name, valid_scientific_names, "scientific_name")
   }
 
   # Check directory
@@ -123,9 +122,9 @@ download_dataset_old <- function(connection = con,
   # TAGS
   message("* (2/6): downloading tags.csv")
   # Select on tags associated with animals
-  tag_ids <-
+  tag_serial_numbers <-
     animals %>%
-    distinct(.data$tag_id) %>%
+    distinct(.data$tag_serial_number) %>%
     pull() %>%
     # To parse out multiple tags (e.g. "A69-9006-904,A69-9006-903"), combine
     # all tags and split them again on comma
@@ -135,15 +134,14 @@ download_dataset_old <- function(connection = con,
     unique()
   tags <- get_tags(
     connection = connection,
-    tag_id = tag_ids,
-    include_ref_tags = TRUE
+    tag_serial_number = tag_serial_numbers
   )
   write_csv(tags, paste(directory, "tags.csv", sep = "/"), na = "")
 
   # DETECTIONS
   message("* (3/6): downloading detections.csv")
   # Select on animal_project_code and scientific_name
-  detections <- get_detections(
+  detections <- get_acoustic_detections(
     connection = connection,
     animal_project_code = animal_project_code,
     scientific_name = scientific_name,
@@ -153,21 +151,21 @@ download_dataset_old <- function(connection = con,
   detections_orig_count <- nrow(detections)
   detections <-
     detections %>%
-    distinct(.data$pk, .keep_all = TRUE)
+    distinct(.data$detection_id, .keep_all = TRUE)
   write_csv(detections, paste(directory, "detections.csv", sep = "/"), na = "")
 
   # DEPLOYMENTS
   message("* (4/6): downloading deployments.csv")
-  # Select on network_project_codes found in detections to get all deployments,
+  # Select on acoustic_project_codes found in detections to get all deployments,
   # including those without detections for animal_project_code
-  network_project_codes <-
+  acoustic_project_codes <-
     detections %>%
-    distinct(.data$network_project_code) %>%
-    arrange(.data$network_project_code) %>%
+    distinct(.data$acoustic_project_code) %>%
+    arrange(.data$acoustic_project_code) %>%
     pull()
-  deployments <- get_deployments(
+  deployments <- get_acoustic_deployments(
     connection = connection,
-    network_project_code = network_project_codes,
+    acoustic_project_code = acoustic_project_codes,
     open_only = FALSE
   )
   # Remove linebreaks in deployment comments to get single lines in csv:
@@ -183,7 +181,7 @@ download_dataset_old <- function(connection = con,
     deployments %>%
     distinct(.data$receiver_id) %>%
     pull()
-  receivers <- get_receivers(
+  receivers <- get_acoustic_receivers(
     connection = connection,
     receiver_id = receiver_ids
   )
@@ -216,27 +214,27 @@ download_dataset_old <- function(connection = con,
     message("* last date of detection:      ", NA)
   }
   message("* included scientific names:   ", paste(scientific_names, collapse = ", "))
-  message("* included network projects:   ", paste(network_project_codes, collapse = ", "))
+  message("* included acoustic projects:   ", paste(acoustic_project_codes, collapse = ", "))
   message("")
 
   # Create warnings
   animals_multiple_tags <-
     animals %>%
-    filter(str_detect(.data$tag_id, ",")) %>%
+    filter(str_detect(.data$tag_serial_number, ",")) %>%
     distinct(.data$animal_id) %>% # Should be unique already
     pull()
 
   tags_multiple_animals <-
     animals %>%
-    group_by(.data$tag_id) %>%
+    group_by(.data$tag_serial_number) %>%
     filter(n() > 1) %>%
-    distinct(.data$tag_id) %>%
+    distinct(.data$tag_serial_number) %>%
     pull()
 
   orphaned_deployments <-
     detections %>%
-    filter(.data$network_project_code %in% c("no_info", "none")) %>%
-    distinct(.data$deployment_fk) %>%
+    filter(.data$acoustic_project_code %in% c("no_info", "none")) %>%
+    distinct(.data$deployment_id) %>%
     pull()
 
   duplicate_detections_count <- detections_orig_count - nrow(detections)
@@ -248,7 +246,7 @@ download_dataset_old <- function(connection = con,
     warning("Found tags associated with multiple animals: ", paste(tags_multiple_animals, collapse = ", "))
   }
   if (length(orphaned_deployments) > 0) {
-    warning("Found deployments without network project: ", paste(orphaned_deployments, collapse = ", "))
+    warning("Found deployments without acoustic project: ", paste(orphaned_deployments, collapse = ", "))
   }
   if (duplicate_detections_count > 0) {
     warning("Found and removed duplicate detections: ", duplicate_detections_count, " detections")
