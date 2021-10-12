cba6c8f72377df02ac1b75abf7436c66c16d969a
peterdesmet <peter.desmet@inbo.be>
2021-10-12 10:37:46 +0200

    Optimize list_ tests and expect !is.na()

--- tests/testthat/test-list_animal_project_codes.R
+++ tests/testthat/test-list_animal_project_codes.R
@@ -1,11 +1,14 @@
 con <- connect_to_etn()
 
 test_that("list_animal_project_codes() returns unique list of values", {
-  expect_is(list_animal_project_codes(con), "character")
-  expect_false(any(duplicated(list_animal_project_codes(con))))
-  expect_true("2014_demer" %in% list_animal_project_codes(con))
+  vector <- list_animal_project_codes(con)
 
+  expect_is(vector, "character")
+  expect_false(any(duplicated(vector)))
+  expect_true(all(!is.na(vector)))
+
+  expect_true("2014_demer" %in% vector)
   # Should not include network or cpod projects
-  expect_false("demer" %in% list_animal_project_codes(con))
-  expect_false("cpod-lifewatch" %in% list_animal_project_codes(con))
+  expect_false("demer" %in% vector)
+  expect_false("cpod-lifewatch" %in% vector)
 })
--- tests/testthat/test-list_cpod_project_codes.R
+++ tests/testthat/test-list_cpod_project_codes.R
@@ -1,11 +1,14 @@
 con <- connect_to_etn()
 
 test_that("list_cpod_project_codes() returns unique list of values", {
-  expect_is(list_cpod_project_codes(con), "character")
-  expect_false(any(duplicated(list_cpod_project_codes(con))))
-  expect_true("cpod-lifewatch" %in% list_cpod_project_codes(con))
+  vector <- list_cpod_project_codes(con)
 
+  expect_is(vector, "character")
+  expect_false(any(duplicated(vector)))
+  expect_true(all(!is.na(vector)))
+
+  expect_true("cpod-lifewatch" %in% vector)
   # Should not include animal or network projects
-  expect_false("2014_demer" %in% list_cpod_project_codes(con))
-  expect_false("demer" %in% list_cpod_project_codes(con))
+  expect_false("2014_demer" %in% vector)
+  expect_false("demer" %in% vector)
 })
