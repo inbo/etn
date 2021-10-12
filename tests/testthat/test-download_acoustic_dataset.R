f20c8cde5cba3b7b33e8c594cf922e5bf89a08f0
peterdesmet <peter.desmet@inbo.be>
2021-10-12 15:35:24 +0200

    Update download_ test

--- tests/testthat/test-download_acoustic_dataset.R
+++ tests/testthat/test-download_acoustic_dataset.R
@@ -1,12 +1,8 @@
-con <- connect_to_etn(
-  username = Sys.getenv("userid"),
-  password = Sys.getenv("pwd")
-)
+con <- connect_to_etn()
 
-test_that("Downloading the data package returns desired message and files", {
-  # Arrange
-  dir_to_download_data <- "./temp_download"
-  dir.create(dir_to_download_data, recursive = TRUE, showWarnings = FALSE)
+test_that("download_acoustic_dataset() creates the expected messages and files", {
+  download_dir <- "./temp_download"
+  dir.create(download_dir, recursive = TRUE, showWarnings = FALSE)
   files_to_create <- c(
     "animals.csv",
     "tags.csv",
@@ -15,46 +11,36 @@ test_that("Downloading the data package returns desired message and files", {
     "receivers.csv",
     "datapackage.json"
   )
-  message_2014_demer <- readLines(
-    "./test-download_dataset-message.txt"
-  )
+  message <- readLines("./test-download_acoustic_dataset-message.txt")
   # Process output message
-  message_2014_demer <- paste0(
-    message_2014_demer, "\n")
-  message_2014_demer <- c(
-    paste0(message_2014_demer[1], message_2014_demer[2]),
-    message_2014_demer[3:length(message_2014_demer)])
+  message <- paste0(message, "\n")
+  message <- c(paste0(message[1], message[2]), message[3:length(message)])
 
-  # Act
-  evaluate_download_2014_demer <- evaluate_promise({
-    download_dataset_old(connection = con,
-                     animal_project_code = "2014_demer",
-                     directory = dir_to_download_data)
+  # Run function
+  evaluate_download <- evaluate_promise({
+    download_acoustic_dataset(
+      con,
+      animal_project_code = "2014_demer",
+      directory = download_dir
+    )
   })
 
-  # Assert
-
   # Function creates the expected files
-  expect_true(
-    all(sort(list.files(dir_to_download_data)) == sort(files_to_create))
-  )
+  expect_true(all(sort(list.files(download_dir)) == sort(files_to_create)))
 
   # Function returns the expected output message
-  expect_true(
-    all(evaluate_download_2014_demer$messages == message_2014_demer)
-  )
+  expect_true(all(evaluate_download$messages == message))
 
   # Function returns no warnings (character of length 0)
-  expect_true(length(evaluate_download_2014_demer$warnings) == 0)
+  expect_true(length(evaluate_download$warnings) == 0)
 
   # Function returns no result
-  expect_null(evaluate_download_2014_demer$result)
+  expect_null(evaluate_download$result)
 
   # Function returns a verbose output of length 1
-  expect_equal(length(evaluate_download_2014_demer$output), 1)
-  expect_true(evaluate_download_2014_demer$output[1] != "")
+  expect_equal(length(evaluate_download$output), 1)
+  expect_true(evaluate_download$output[1] != "")
 
   # Remove generated files and directories after test
-  unlink(dir_to_download_data, recursive = TRUE)
-  unlink("./data_test_download_dataset/detections.csv")
+  unlink(download_dir, recursive = TRUE)
 })
