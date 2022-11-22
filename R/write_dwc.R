882b9306be43870b511737d0fa32ac395710e3a1
peterdesmet <peter.desmet@inbo.be>
2022-11-22 16:14:30 +0100

    Update write_dwc() to download human obs

--- R/write_dwc.R
+++ R/write_dwc.R
@@ -10,10 +10,70 @@
 #' @param animal_project_code Animal project code.
 #' @param directory Path to local directory to write file(s) to.
 #'   Existing files of the same name will be overwritten.
+#' @param rights_holder Acronym of the organization owning or managing the
+#'   rights over the data.
+#' @param license Identifier of the license under which the data will be
+#'   published.
+#'   - [`CC-BY-4.0`](https://creativecommons.org/licenses/by/4.0/legalcode)
+#'   (default).
+#'   - [`CC0-1.0`](https://creativecommons.org/publicdomain/zero/1.0/legalcode).
 #' @return CSV file(s) written to disk.
 #' @export
 write_dwc <- function(connection = con,
                       animal_project_code,
-                      directory = animal_project_code) {
-  message("functionality here")
+                      directory = animal_project_code,
+                      rights_holder = NULL,
+                      license = "CC-BY-4.0") {
+  # Check connection
+  check_connection(connection)
+
+  # Check animal_project_code
+  assertthat::assert_that(
+    length(animal_project_code) == 1,
+    msg = "`animal_project_code` must be a single value."
+  )
+
+  # Check license
+  licenses <- c("CC-BY-4.0", "CC0-1.0")
+  assertthat::assert_that(
+    license %in% licenses,
+    msg = glue::glue(
+      "`license` must be {licenses}.",
+      licenses = glue::glue_collapse(licenses, sep = ", ", last = " or ")
+    )
+  )
+  license <- switch(
+    license,
+    "CC-BY-4.0" = "https://creativecommons.org/licenses/by/4.0/legalcode",
+    "CC0-1.0" = "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
+  )
+
+  # Get imis_dataset_id and project_id from animal_project
+  project <- get_animal_projects(connection, animal_project_code)
+  project_id <- project$project_id
+  dataset_id <- paste0("https://www.vliz.be/en/imis?module=dataset&dasid=", project$imis_dataset_id)
+
+  # Get human observations (capture, release, ...)
+  dwc_occurrence_human_sql <- glue::glue_sql(
+    readr::read_file(system.file("sql/dwc_occurrence_human.sql", package = "etn")),
+    .con = connection
+  )
+  dwc_occurrence_human <- DBI::dbGetQuery(connection, dwc_occurrence_human_sql)
+
+  # Get machine observations (sampled down detections)
+  # dwc_occurrence_machine_sql <- glue::glue_sql(
+  #   readr::read_file(system.file("sql/dwc_occurrence_machine.sql", package = "etn")),
+  #   .con = connection
+  # )
+  # dwc_occurrence_machine <- DBI::dbGetQuery(connection, dwc_occurrence_machine_sql)
+
+  # Create directory
+  dir.create(directory, recursive = TRUE, showWarnings = FALSE)
+
+  # Write files
+  readr::write_csv(
+    dwc_occurrence_human,
+    file.path(directory, "dwc_occurrence_human.csv"),
+    na = ""
+  )
 }
--- inst/sql/dwc_occurrence_human.sql
+++ inst/sql/dwc_occurrence_human.sql
@@ -2,9 +2,12 @@
 Schema: https://rs.gbif.org/core/dwc_occurrence_2022-02-02.xml
 */
 
--- The animals table contains multiple (human observation) events (capture, release, surgery, recapture) as columns.
--- Here they are transposed to rows. Events without date information are excluded.
-WITH animal_events AS (
+/* HELPER TABLE FOR HUMAN OBSERVATION EVENTS
+
+The animals table contains multiple events (capture, release, surgery, recapture) as columns.
+Here they are transposed to rows. Events without date information are excluded.
+*/
+WITH event AS (
   SELECT *
   FROM
     (
@@ -12,7 +15,7 @@ WITH animal_events AS (
         animal.id_pk                    AS id_pk,
         'capture'                       AS protocol,
         animal.catched_date_time        AS date,
-        animal.capture_location         AS location,
+        animal.capture_location         AS locality,
         animal.capture_latitude         AS latitude,
         animal.capture_longitude        AS longitude
       FROM common.animal_release_limited AS animal
@@ -24,7 +27,7 @@ WITH animal_events AS (
         animal.id_pk                    AS id_pk,
         'surgery'                       AS protocol,
         animal.date_of_surgery          AS date,
-        animal.surgery_location         AS location,
+        animal.surgery_location         AS locality,
         animal.surgery_latitude         AS latitude,
         animal.surgery_longitude        AS longitude
       FROM common.animal_release_limited AS animal
@@ -36,7 +39,7 @@ WITH animal_events AS (
         animal.id_pk                    AS id_pk,
         'release'                       AS protocol,
         animal.utc_release_date_time    AS date,
-        animal.release_location         AS location,
+        animal.release_location         AS locality,
         animal.release_latitude         AS latitude,
         animal.release_longitude        AS longitude
       FROM common.animal_release_limited AS animal
@@ -48,7 +51,7 @@ WITH animal_events AS (
         animal.id_pk                    AS id_pk,
         'recapture'                     AS protocol,
         animal.recapture_date           AS date,
-        NULL                            AS location,
+        NULL                            AS locality,
         NULL                            AS latitude,
         NULL                            AS longitude
       FROM common.animal_release_limited AS animal
@@ -62,6 +65,74 @@ WITH animal_events AS (
 )
 
 SELECT
-  *
+-- RECORD LEVEL
+  'Event'                               AS type,
+  {license}                             AS license,
+  {rights_holder}                       AS rightsHolder,
+  {dataset_id}                          AS datasetID,
+  'VLIZ'                                AS institutionCode,
+  'ETN'                                 AS collectionCode,
+-- datasetName
+-- RECORD LEVEL
+  'HumanObservation'                    AS basisOfRecord,
+  NULL                                  AS dataGeneralizations,
+-- OCCURRENCE
+  animal.id_pk || '_' || tag_device.serial_number || '_' || event.protocol AS occurrenceID, -- Same as EventID
+  animal.sex                            AS sex,
+  animal.life_stage                     AS lifeStage,
+  'present'                             AS occurrenceStatus,
+  animal.id_pk                          AS organismID,
+  animal.animal_nickname                AS organismName,
+-- EVENT
+  animal.id_pk || '_' || tag_device.serial_number || '_' || event.protocol AS EventID,
+  animal.id_pk || '_' || tag_device.serial_number AS parentEventID,
+  to_char(event.date, 'YYYY-MM-DD"T"HH:MI:SS"Z"') AS eventDate,
+  event.protocol                        AS samplingProtocol,
+  CASE
+    WHEN event.protocol = 'capture' THEN
+      'Caugth using ' || TRIM(LOWER(animal.capture_method))
+    WHEN event.protocol = 'release' THEN
+      manufacturer.project || ' ' || tag_device.model || ' tag ' ||
+      CASE
+        WHEN LOWER(animal.implant_type) = 'internal' THEN 'implanted in '
+        WHEN LOWER(animal.implant_type) = 'external' THEN 'attached to '
+        ELSE 'implanted in/attached to ' -- Includes "Acoutic and pit", "internal and external"
+      END ||
+      CASE
+        WHEN LOWER(animal.wild_or_hatchery) = 'wild' OR LOWER(animal.wild_or_hatchery) = 'w' THEN 'free-ranging animal'
+        WHEN LOWER(animal.wild_or_hatchery) = 'hatchery' OR LOWER(animal.wild_or_hatchery) = 'h' THEN 'hatched animal'
+        ELSE 'likely free-ranging animal'
+      END
+    ELSE NULL
+  END                                   AS eventRemarks,
+-- LOCATION
+  NULL                                  AS locationID,
+  event.locality                        AS locality,
+  event.latitude                        AS decimalLatitude,
+  event.longitude                       AS decimalLongitude,
+  CASE
+    WHEN event.latitude IS NOT NULL THEN 'WGS84'
+    ELSE NULL
+  END                                   AS geodeticDatum,
+  CASE
+    WHEN event.latitude IS NOT NULL THEN 1000 -- Event coordinates not always precise
+    ELSE NULL
+  END                                   AS coordinateUncertaintyInMeters,
+-- TAXON
+  'urn:lsid:marinespecies.org:taxname:' || animal.aphia_id AS scientificNameID,
+  animal.scientific_name                AS scientificName,
+  'Animalia'                            AS kingdom
 FROM
-  animal_events
+  event
+  LEFT JOIN common.animal_release_limited AS animal
+    ON event.id_pk = animal.id_pk
+    LEFT JOIN common.animal_release_tag_device AS animal_with_tag
+      ON animal.id_pk = animal_with_tag.animal_release_fk
+      LEFT JOIN common.tag_device_limited AS tag_device
+        ON animal_with_tag.tag_device_fk = tag_device.id_pk
+        LEFT JOIN common.tag_device_type AS tag_type
+          ON tag_device.tag_device_type_fk = tag_type.id_pk
+        LEFT JOIN common.manufacturer AS manufacturer
+          ON tag_device.manufacturer_fk = manufacturer.id_pk
+WHERE
+  animal.project_fk = {animal_project_id}
