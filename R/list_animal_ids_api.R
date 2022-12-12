03b1348e974adac6e5fcd0e8171c50d309c147b9
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2022-12-12 10:42:58 +0100

    comment out unused code, warn for status, add roxygen skeleton

--- R/list_animal_ids_api.R
+++ R/list_animal_ids_api.R
@@ -1,92 +1,78 @@
-# library(etn)
-# con <- connect_to_etn()
-# write_dwc(
-#   animal_project_code = "2014_demer")
-#
 
-library(httr)
+
+# library(httr)
 
 # set access credentials --------------------------------------------------
 
 
 
-Sys.setenv(userid = "pieter.huybrechts@inbo.be")
-Sys.setenv(pwd = askpass::askpass())
+# Sys.setenv(userid = "pieter.huybrechts@inbo.be")
+# Sys.setenv(pwd = askpass::askpass())
 
-# function to get credentials
 
-#' Get the credentials from environement variables, or set them manually
-#'
-#' By default, it's not neccesairy to set any values in this function as it's
-#' used in the background by other functions. However, if you wish to provide
-#' your username and password on a per function basis, this function allows you
-#' to do so.
-#'
-#' @param username ETN Data username, by default read from the environement, but
-#'   you can set it manually too.
-#' @param password ETN Data password, by default read from the environement, but
-#'   you can set it manually too.
-#'
-#' @return A string as it is ingested by other functions that need
-#'   authentification
-#' @export
-#'
-#' @examples
-get_credentials <-
-  function(username = Sys.getenv("userid"),
-           password = Sys.getenv("pwd")) {
-    stringr::str_glue('list(username = "{username}", password = "{password}")')
-  }
+
 
 # try the opencpu API --------------------------------------------------------
 
-# Dont Run, little draft to explore possibilities
-while(FALSE){
-api_domain <- "https://opencpu.lifewatch.be"
-function_path <- "library/etn/R/list_animal_ids"
-userid <- "pieter.huybrechts@inbo.be"
-
-
-# pass credentials/function arguments as a string
-response <-
-  POST(file.path(api_domain,
-       function_path),
-       body = list(
-         con = stringr::str_glue(
-           "list(username='{userid}', password='{askpass::askpass()}')",
-         )
-       ))
-
-content(response, as = "text")
-
-extract_temp_key <- function(response){
-  response %>%
-    httr::content(as = "text") %>%
-    stringr::str_extract("(?<=tmp\\/).{15}(?=\\/)")
-}
-
-temp_key <- extract_temp_key(response)
-
-}
+# # Dont Run, little draft to explore possibilities
+# while(FALSE){
+# api_domain <- "https://opencpu.lifewatch.be"
+# function_path <- "library/etn/R/list_animal_ids"
+# userid <- "pieter.huybrechts@inbo.be"
+#
+#
+# # pass credentials/function arguments as a string
+# response <-
+#   POST(file.path(api_domain,
+#        function_path),
+#        body = list(
+#          con = stringr::str_glue(
+#            "list(username='{userid}', password='{askpass::askpass()}')",
+#          )
+#        ))
+#
+# content(response, as = "text")
+#
+# extract_temp_key <- function(response){
+#   response %>%
+#     httr::content(as = "text") %>%
+#     stringr::str_extract("(?<=tmp\\/).{15}(?=\\/)")
+# }
+#
+# temp_key <- extract_temp_key(response)
+#
+# }
 # retreive as json directly in a single call -----------------------------------
 
 
 # as a function
 
-list_animal_ids <- function(credentials = get_credentials()) {
+#' @inheritSection list_animal_ids title
+#' @inheritSection list_animal_ids description
+#'
+#' @param credentials Login credentials to the ETN database, as created by
+#'   `get_credentials()`
+#' @inheritSection list_animal_ids return
+#' @export
+#'
+list_animal_ids_api <- function(credentials = get_credentials()) {
   api_domain <- "https://opencpu.lifewatch.be"
   function_path <- "library/etn/R/list_animal_ids"
 
+  # POST the function request to the api_domain, request json as direct return
   response <-
-    POST(stringr::str_glue(api_domain,function_path,"json",.sep = "/"),
+    httr::POST(stringr::str_glue(api_domain,function_path,"json",.sep = "/"),
          body = list(
            con = credentials
          ))
+  # If request was not succesful, generate a warning
+  httr::warn_for_status(response, "submit request to API server")
+
+  # Parse server response JSON to a vector
   response %>%
     httr::content(as = "text", encoding = "UTF-8") %>%
     jsonlite::fromJSON(simplifyVector = TRUE)
 }
-
 
 # two step alternative, POST then GET result ------------------------------
 
@@ -123,11 +109,11 @@ get_val <- function(temp_key, api_domain = "https://opencpu.lifewatch.be"){
 ## test case with rnorm ----------------------------------------------------
 # api_domain <- "https://cloud.opencpu.org/ocpu"
 # post request
-response <- httr::POST("https://cloud.opencpu.org/ocpu/library/stats/R/rnorm",
-           body = list(n = 10,
-                       mean = 5))
-
-response %>%
-  extract_temp_key() %>%
-  get_val(api_domain = "https://cloud.opencpu.org/ocpu")
-
+# response <- httr::POST("https://cloud.opencpu.org/ocpu/library/stats/R/rnorm",
+#            body = list(n = 10,
+#                        mean = 5))
+#
+# response %>%
+#   extract_temp_key() %>%
+#   get_val(api_domain = "https://cloud.opencpu.org/ocpu")
+#
