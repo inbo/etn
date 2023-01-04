420f9d5da2d1d48fb7b7599d3e55210189790e70
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-01-04 14:17:19 +0100

    don't use two statics, but only one
    See https://github.com/inbo/etn/pull/271/files/e24e20d7998e5d17a996bf63fbfb79b7551b155e#r1061471137

--- R/list_animal_ids_api.R
+++ R/list_animal_ids_api.R
@@ -5,12 +5,10 @@
 #' @return A vector of all unique `id_pk` present in `common.animal_release`.
 #' @export
 list_animal_ids_api <- function(credentials = get_credentials()) {
-  api_domain <- "https://opencpu.lifewatch.be"
-  function_path <- "library/etn/R/list_animal_ids"
-
+  endpoint <- "https://opencpu.lifewatch.be/library/etn/R/list_animal_ids"
   # POST the function request to the api_domain, request json as direct return
   response <-
-    httr::POST(paste(api_domain, function_path, "json", sep = "/"),
+    httr::POST(paste(endpoint, "json", sep = "/"),
       body = list(
         con = credentials
       )
