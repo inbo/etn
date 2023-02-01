b9a994459fc33b21c521c3969cdc555fffde1d5c
PietrH <pieterhuy@gmail.com>
2023-02-01 16:04:28 +0100

    replace expect_is to conform with testthat 3e

--- tests/testthat/test-list_scientific_names.R
+++ tests/testthat/test-list_scientific_names.R
@@ -3,7 +3,7 @@ con <- connect_to_etn()
 test_that("list_scientific_names() returns unique list of values", {
   vector <- list_scientific_names(con)
 
-  expect_is(vector, "character")
+  expect_type(vector, "character")
   expect_false(any(duplicated(vector)))
   expect_true(all(!is.na(vector)))
 
--- tests/testthat/test-list_tag_serial_numbers.R
+++ tests/testthat/test-list_tag_serial_numbers.R
@@ -3,7 +3,7 @@ con <- connect_to_etn()
 test_that("list_tag_serial_numbers() returns unique list of values", {
   vector <- list_tag_serial_numbers(con)
 
-  expect_is(vector, "character")
+  expect_type(vector, "character")
   expect_false(any(duplicated(vector)))
   expect_true(all(!is.na(vector)))
 
