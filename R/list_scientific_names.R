d57132241fa76c7dcbf1ecfe584d937a2543f79e
PietrH <pieterhuy@gmail.com>
2023-06-08 16:33:19 +0200

    adapt list_scientific_names() and list_station_names() to use api

--- R/list_scientific_names.R
+++ R/list_scientific_names.R
@@ -6,7 +6,39 @@
 #'   `common.animal_release`.
 #'
 #' @export
-list_scientific_names <- function(connection = con) {
+list_scientific_names <- function(api = TRUE,
+                                  connection) {
+  # check arguments
+  assertthat::assert_that(assertthat::is.flag(api))
+  # Lock in the name of the parent function
+  function_identity <-
+    get_parent_fn_name()
+
+  # the connection argument has been depriciated
+  if (lifecycle::is_present(connection)) {
+    deprecate_warn_connection(function_identity)
+  }
+
+  arguments_to_pass <-
+    return_parent_arguments()[
+      !names(return_parent_arguments()) %in% c("api", "connection", "function_identity")]
+
+  if(api){
+    out <- do.call(
+      forward_to_api,
+      list(function_identity = function_identity, payload = arguments_to_pass)
+    )
+  } else {
+    out <- do.call(glue::glue("{function_identity}_sql"), arguments_to_pass)
+  }
+  return(out)
+}
+#' list_scientific_names() sql helper
+#'
+#' @inheritParams list_scientific_names()
+#' @noRd
+#'
+list_scientific_names_sql <- function(){
   query <- glue::glue_sql(
     "SELECT DISTINCT scientific_name FROM common.animal_release",
     .con = connection
--- R/list_station_names.R
+++ R/list_station_names.R
@@ -6,7 +6,44 @@
 #'   `acoustic.deployments`.
 #'
 #' @export
-list_station_names <- function(connection = con) {
+list_station_names <- function(api = TRUE,
+                               connection) {
+  # check arguments
+  assertthat::assert_that(assertthat::is.flag(api))
+  # Lock in the name of the parent function
+  function_identity <-
+    get_parent_fn_name()
+
+  # the connection argument has been depriciated
+  if (lifecycle::is_present(connection)) {
+    deprecate_warn_connection(function_identity)
+  }
+
+  arguments_to_pass <-
+    return_parent_arguments()[
+      !names(return_parent_arguments()) %in% c("api", "connection", "function_identity")]
+
+  if(api){
+    out <- do.call(
+      forward_to_api,
+      list(function_identity = function_identity, payload = arguments_to_pass)
+    )
+  } else {
+    out <- do.call(glue::glue("{function_identity}_sql"), arguments_to_pass)
+  }
+  return(out)
+}
+#' list_station_names() sql helper
+#'
+#' @inheritParams list_station_names()
+#' @noRd
+#'
+list_station_names_sql <- function(){
+  # Create connection
+  connection <- do.call(connect_to_etn, get_credentials())
+  # Check connection
+  check_connection(connection)
+
   query <- glue::glue_sql(
     "SELECT DISTINCT station_name FROM acoustic.deployments WHERE station_name IS NOT NULL",
     .con = connection
