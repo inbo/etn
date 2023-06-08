0ef96bd3166532b2912c6d07d173ae652433a755
PietrH <pieterhuy@gmail.com>
2023-06-08 16:27:04 +0200

    rewrite list_animal_project_codes() and list_cereiver_ids() to use api

--- R/list_receiver_ids.R
+++ R/list_receiver_ids.R
@@ -5,7 +5,46 @@
 #' @return A vector of all unique `receiver` present in `acoustic.receivers`.
 #'
 #' @export
-list_receiver_ids <- function(connection = con) {
+list_receiver_ids <- function(api = TRUE,
+                              connection){
+  # check arguments
+  assertthat::assert_that(assertthat::is.flag(api))
+  # Lock in the name of the parent function
+  function_identity <-
+    get_parent_fn_name()
+
+  # the connection argument has been depriciated
+  if (lifecycle::is_present(connection)) {
+    deprecate_warn_connection(function_identity)
+  }
+
+  arguments_to_pass <-
+    return_parent_arguments()[
+      !names(return_parent_arguments()) %in% c("api", "connection", "function_identity")]
+
+  if(api){
+    out <- do.call(
+      forward_to_api,
+      list(function_identity = function_identity, payload = arguments_to_pass)
+    )
+  } else {
+    out <- do.call(glue::glue("{function_identity}_sql"), arguments_to_pass)
+  }
+  return(out)
+}
+
+
+#' list_receiver_ids() sql helper
+#'
+#' @inheritParams list_receiver_ids()
+#' @noRd
+#'
+list_receiver_ids_sql <- function(){
+  # Create connection
+  connection <- do.call(connect_to_etn, get_credentials())
+  # Check connection
+  check_connection(connection)
+
   query <- glue::glue_sql(
     "SELECT DISTINCT receiver FROM acoustic.receivers",
     .con = connection
--- tests/testthat/test-list_receiver_ids.R
+++ tests/testthat/test-list_receiver_ids.R
@@ -1,9 +1,7 @@
-con <- connect_to_etn()
-
 test_that("list_receiver_ids() returns unique list of values", {
-  vector <- list_receiver_ids(con)
+  vector <- list_receiver_ids()
 
-  expect_is(vector, "character")
+  expect_type(vector, "character")
   expect_false(any(duplicated(vector)))
   expect_true(all(!is.na(vector)))
 
