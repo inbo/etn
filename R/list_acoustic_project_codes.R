263a8bc44b8eecea137c18b4957959349c8f77b4
PietrH <pieterhuy@gmail.com>
2023-06-08 15:54:39 +0200

    rewrite list_acoustic_project_codes() to use api

--- R/list_acoustic_project_codes.R
+++ R/list_acoustic_project_codes.R
@@ -6,7 +6,43 @@
 #'   `project.sql`.
 #'
 #' @export
-list_acoustic_project_codes <- function(connection = con) {
+list_acoustic_project_codes <- function(api = TRUE,
+                                        connection) {
+  # Lock in the name of the parent function
+  function_identity <-
+    get_parent_fn_name()
+
+  # the connection argument has been depriciated
+  if (lifecycle::is_present(connection)) {
+    deprecate_warn_connection(function_identity)
+  }
+
+  arguments_to_pass <-
+    return_parent_arguments()[
+      !names(return_parent_arguments()) %in% c("api", "connection", "function_identity")]
+
+  if(api){
+    out <- do.call(
+      forward_to_api,
+      list(function_identity = function_identity, payload = arguments_to_pass)
+    )
+  } else {
+    out <- do.call(glue::glue("{function_identity}_sql"), arguments_to_pass)
+  }
+  return(out)
+}
+
+#' list_acoustic_project_codes() sql helper
+#'
+#' @inheritParams list_acoustic_project_codes()
+#' @noRd
+#'
+list_acoustic_project_codes_sql <- function(){
+  # Create connection
+  connection <- do.call(connect_to_etn, get_credentials())
+  # Check connection
+  check_connection(connection)
+
   project_sql <- glue::glue_sql(
     readr::read_file(system.file("sql", "project.sql", package = "etn")),
     .con = connection
