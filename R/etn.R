aec437691c3a604ad744edbea3b3f4bfdd23014e
PietrH <pieterhuy@gmail.com>
2023-06-15 16:08:38 +0200

    hard deprecate `connect_to_etn()`, use lifecycle for badges. Add to imports.

--- R/etn.R
+++ R/etn.R
@@ -1,3 +1,8 @@
 #' @importFrom dplyr %>% .data distinct filter pull
 #' @keywords internal
 "_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom lifecycle deprecated
+## usethis namespace: end
+NULL
