dfa8bdbb262271a70eead73f5d43105ab9b568b5
PietrH <pieterhuy@gmail.com>
2023-06-15 15:34:53 +0200

    adapt list_tag_serial_numbers() to use api

--- R/list_tag_serial_numbers.R
+++ R/list_tag_serial_numbers.R
@@ -6,7 +6,29 @@
 #'   `common.tag_device`.
 #'
 #' @export
-list_tag_serial_numbers <- function(connection = con) {
+list_tag_serial_numbers <- function(api = TRUE,
+                                    connection) {
+  # Check arguments
+  # The connection argument has been depreciated
+  if (lifecycle::is_present(connection)) {
+    deprecate_warn_connection()
+  }
+  # Either use the API, or the SQL helper.
+  out <- conduct_parent_to_helpers(api)
+  return(out)
+}
+
+#' list_tag_serial_numbers() sql helper
+#'
+#' @inheritParams list_tag_serial_numbers()
+#' @noRd
+#'
+list_tag_serial_numbers_sql <- function() {
+  # Create connection
+  connection <- do.call(connect_to_etn, get_credentials())
+  # Check connection
+  check_connection(connection)
+
   query <- glue::glue_sql(
     "SELECT DISTINCT serial_number FROM common.tag_device",
     .con = connection
--- tests/testthat/test-list_tag_serial_numbers.R
+++ tests/testthat/test-list_tag_serial_numbers.R
@@ -1,11 +1,17 @@
-con <- connect_to_etn()
 
 test_that("list_tag_serial_numbers() returns unique list of values", {
-  vector <- list_tag_serial_numbers(con)
+  vector <- list_tag_serial_numbers()
 
-  expect_is(vector, "character")
+  expect_type(vector, "character")
   expect_false(any(duplicated(vector)))
   expect_true(all(!is.na(vector)))
 
   expect_true("1187450" %in% vector)
+
+  vector_sql <- list_tag_serial_numbers(api = FALSE)
+  expect_type(vector_sql, "character")
+  expect_false(any(duplicated(vector_sql)))
+  expect_true(all(!is.na(vector_sql)))
+
+  expect_true("1187450" %in% vector_sql)
 })
