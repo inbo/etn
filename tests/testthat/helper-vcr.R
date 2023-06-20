4455e0f8b2fa8642859d3c2048d454c3610166b1
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-06-20 14:34:01 +0200

    hide secrets

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -1,5 +1,7 @@
 library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
+  filter_sensitive_data = list("<<<my_userid>>>" = Sys.getenv('userid'),
+                               "<<<my_pwd>>>" = Sys.getenv('pwd')),
   dir = vcr::vcr_test_path("fixtures")
 ))
 vcr::check_cassette_names()
