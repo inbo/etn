037f241efacd2acec80f0c009348f62428f16e20
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-06-20 15:21:40 +0200

    use vcr to cache response

--- R/list_animal_ids.R
+++ R/list_animal_ids.R
@@ -12,7 +12,7 @@ list_animal_ids <- function(api = TRUE,
     deprecate_warn_connection()
   }
   # Either use the API, or the SQL helper.
-  out <- conduct_parent_to_helpers(api)
+  out <- conduct_parent_to_helpers(api, json = TRUE)
   return(out)
 }
 
