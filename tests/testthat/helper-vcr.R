d2b9cf95e45693d2a07c98feef688d67b2baad8f
PietrH <pieterhuy@gmail.com>
2024-09-13 16:44:34 +0200

    Filtering opencpu temp key doesn't stop mismatching on uri

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -2,8 +2,6 @@ library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
   filter_sensitive_data = list("<<<my_userid>>>" = Sys.getenv('userid'),
                                "<<<my_pwd>>>" = Sys.getenv('pwd')),
-  filter_sensitive_data_regex =
-    list("\\1<<<opencpu_temp_key>>>" = "(tmp\\/)[a-z0-9]*"),
   dir = vcr::vcr_test_path("fixtures")
 ))
 vcr::check_cassette_names()
