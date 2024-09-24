95075c058f24c817912e7ae2e6ea57da6f741fd0
PietrH <pieterhuy@gmail.com>
2024-09-24 09:08:25 +0200

    update test reference for `list_animal_ids()`

--- tests/testthat/test-list_animal_ids.R
+++ tests/testthat/test-list_animal_ids.R
@@ -1,4 +1,5 @@
 vcr::use_cassette("list_animal_ids", result_api <- list_animal_ids())
+result_sql <- list_animal_ids(api = FALSE)
 
 test_that("list_animal_ids() returns unique list of values using api", {
   expect_type(result_api, "integer")
@@ -7,7 +8,6 @@ test_that("list_animal_ids() returns unique list of values using api", {
 })
 
 test_that("list_animal_ids() returns same list over api and sql", {
-  result_sql <- list_animal_ids(api = FALSE)
   expect_identical(result_api, result_sql)
 })
 
@@ -16,6 +16,7 @@ test_that("list_animal_ids returns at least 5 known values", {
   known_ids <- c("56314", "8504", "7601", "4293", "58407")
 
   testthat::expect_true(all(known_ids %in% result_api))
+  testthat::expect_true(all(known_ids %in% result_sql))
 })
 
 test_that("list_animal_ids() warns for depreciation of connection", {
