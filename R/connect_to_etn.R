0f922c45baf158d80ae431860fbae097a9cc8a77
PietrH <pieterhuy@gmail.com>
2024-09-25 15:35:16 +0200

    Remove example, users should no longer use this function.

--- R/connect_to_etn.R
+++ R/connect_to_etn.R
@@ -9,16 +9,6 @@
 #' @return ODBC connection to ETN database.
 #'
 #' @export
-#'
-#' @examples
-#' \dontrun{
-#' # Connect to the ETN database using your rstudio.lifewatch.be username and
-#' # password, and save as the default connection variable "con"
-#' con <- connect_to_etn()
-#'
-#' # Connect to the ETN database using non-default username and password
-#' con <- connect_to_etn(username = "my_username", password = "my_password")
-#' }
 connect_to_etn <- function() {
   lifecycle::deprecate_warn(
     when = "2.3.0",
