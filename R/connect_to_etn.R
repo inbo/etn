2c913c27a3021af7660e8dd1cbd56881a49c3ece
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2024-09-30 10:24:01 +0200

    Correct grammar
    Co-authored-by: Maëlle Salmon <maelle.salmon@yahoo.se>

--- R/connect_to_etn.R
+++ R/connect_to_etn.R
@@ -1,7 +1,7 @@
 #' Connect to the ETN database
 #'
 #' This function is `r lifecycle::badge("deprecated")` since etn version 2.3.0.
-#' It's use is no longer supported or needed. All connections to the ETN database
+#' Its use is no longer supported or needed. All connections to the ETN database
 #' are now made automatically when you use a function. If your credentials are not saved
 #' in the system environement, you will be prompted to enter them.
 #'
