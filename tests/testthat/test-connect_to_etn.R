2de2c431864e4e2fac6b8ecd40e7c074d4a26e13
PietrH <pieterhuy@gmail.com>
2024-09-30 11:01:23 +0200

    Split tests, skip 2nd and 3rd deprecation check because of bug #319

--- tests/testthat/test-connect_to_etn.R
+++ tests/testthat/test-connect_to_etn.R
@@ -1,12 +1,20 @@
-test_that("connect_to_etn() returns deprecation warning when used", {
-  lifecycle::expect_deprecated(
-    connect_to_etn()
-  )
+test_that("connect_to_etn() returns deprecation warning when used with named arguments", {
   lifecycle::expect_deprecated(
     connect_to_etn(username = "my name", password = "my password")
   )
+})
+
+test_that("connect_to_etn() returns deprecation warning when used with unnamed arguments", {
+  skip("BUG #319: tests will always fail after first deprecation test")
   lifecycle::expect_deprecated(
     connect_to_etn("my name", "my password")
+  )
+})
+
+test_that("connect_to_etn() returns deprecation argument when used without arguments",{
+  skip("BUG #319: tests will always fail after first deprecation test")
+  lifecycle::expect_deprecated(
+    connect_to_etn()
   )
 })
 
