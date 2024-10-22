47bf414a64d002d60eb98ed83f27ece77859d367
PietrH <pieterhuy@gmail.com>
2024-10-22 10:02:35 +0200

    remove unused helper -> moved to etnservice

--- R/utils.R
+++ R/utils.R
@@ -47,46 +47,6 @@ check_value <- function(x, y, name = "value", lowercase = FALSE) {
   return(x)
 }
 
-#' Check if the string input can be converted to a date
-#'
-#' Returns `FALSE`` or the cleaned character version of the date
-#' (acknowledgments to micstr/isdate.R).
-#'
-#' @param date_time Character. A character representation of a date.
-#' @param date_name Character. Informative description to user about type of
-#'   date.
-#' @return `FALSE` | character
-#' @family helper functions
-#' @noRd
-#' @examples
-#' \dontrun{
-#' check_date_time("1985-11-21")
-#' check_date_time("1985-11")
-#' check_date_time("1985")
-#' check_date_time("1985-04-31") # invalid date
-#' check_date_time("01-03-1973") # invalid format
-#' }
-check_date_time <- function(date_time, date_name = "start_date") {
-  parsed <- tryCatch(
-    lubridate::parse_date_time(date_time, orders = c("ymd", "ym", "y")),
-    warning = function(warning) {
-      if (grepl("No formats found", warning$message)) {
-        stop(glue::glue(
-          "The given {date_name}, {date_time}, is not in a valid ",
-          "date format. Use a yyyy-mm-dd format or shorter, ",
-          "e.g. 2012-11-21, 2012-11 or 2012."
-        ))
-      } else {
-        stop(glue::glue(
-          "The given {date_name}, {date_time} can not be interpreted ",
-          "as a valid date."
-        ))
-      }
-    }
-  )
-  as.character(parsed)
-}
-
 #' Get the credentials from environment variables, or set them manually
 #'
 #' By default, it's not necessary to set any values in this function as it's
