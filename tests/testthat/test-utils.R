96805a1e94d8107ae0fa3fe95fa0d01b78019b0e
PietrH <pieterhuy@gmail.com>
2024-10-22 10:04:23 +0200

    local database connection is made via etnservice

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -17,21 +17,3 @@ test_that("deprecate_warn_connection() returns warning when connection is provid
     fixed = TRUE
   )
 })
-
-# create_connection() -----------------------------------------------------
-test_that("create_connection() can create a connection with the database", {
-  con <- create_connection()
-  expect_true(check_connection(con))
-  expect_true(isClass(con, "PostgreSQL"))
-})
-
-test_that("create_connection() returns error on non character arguments", {
-  expect_error(
-    create_connection(list(username = 1, password = "password")),
-    regexp = "username is not a string"
-  )
-  expect_error(
-    create_connection(credentials = list(username = "username", password = 1)),
-    regexp = "password is not a string"
-  )
-})
