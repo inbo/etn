a00c3cc713e5a2b311c69033b62ccbfb5ff2b58f
PietrH <pieterhuy@gmail.com>
2025-03-21 14:10:22 +0100

    Move to test helper file

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -26,3 +26,16 @@ fetch_schema_fields <- function(datapackage = datapackage, table_name) {
       "schema"
       ]][["fields"]]
 }
+
+#' Skip the test if ETN is not a local database on this machine.
+#'
+#' This function checks if the "ETN" database is present in the list of ODBC
+#' data sources on the local machine. If it is not found, the test is skipped
+#' with a corresponding message.
+#'
+#' @family helper functions
+#' @noRd
+skip_if_not_localdb <- function(){
+  skip_if_not("ETN" %in% odbc::odbcListDataSources()$name,
+              "ETN is not a local database on this machine")
+}
