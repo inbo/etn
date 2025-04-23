b6c53a855659b20e952b0fb419f5587bcf29423a
PietrH <pieterhuy@gmail.com>
2025-04-23 13:54:37 +0200

    Make tests more independent of one another, minimize code outside of tests

--- tests/testthat/test-list_animal_ids.R
+++ tests/testthat/test-list_animal_ids.R
@@ -1,5 +1,5 @@
 vcr::use_cassette("list_animal_ids", result_api <- list_animal_ids())
-result_sql <- list_animal_ids(api = FALSE)
+
 
 test_that("list_animal_ids() returns unique list of values using api", {
   expect_type(result_api, "integer")
@@ -9,11 +9,15 @@ test_that("list_animal_ids() returns unique list of values using api", {
 
 test_that("list_animal_ids() returns same list over api and sql", {
   skip_if_not_localdb()
+  result_sql <- list_animal_ids(api = FALSE)
+
   expect_identical(result_api, result_sql)
 })
 
 test_that("list_animal_ids returns at least 5 known values", {
   skip_if_not_localdb()
+  result_sql <- list_animal_ids(api = FALSE)
+
   # a set of 5 known id_pk present in common.animal_release
   known_ids <- c("56314", "8504", "7601", "4293", "58407")
 
