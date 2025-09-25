e04e32104a928803ed842ceedffcdc83c07b2f1c
PietrH <pieterhuy@gmail.com>
2025-09-25 16:46:06 +0200

    Don't need to assign object anymore, return direct.

--- R/conduct_parent_to_helpers.R
+++ R/conduct_parent_to_helpers.R
@@ -79,7 +79,7 @@ conduct_parent_to_helpers <- function(protocol = c("opencpu", "localdb"),
             )
         )
       }
-      out <- do.call(utils::getFromNamespace(function_identity, ns = "etnservice"),
+      do.call(utils::getFromNamespace(function_identity, ns = "etnservice"),
         args = append(arguments_to_pass,
           list(credentials = get_credentials()),
           after = 0
