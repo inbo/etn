f17e88f17f88a194fc036c39cd32f06f723fc9a5
PietrH <pieterhuy@gmail.com>
2025-09-25 15:59:16 +0200

    Update roxygen docs after argument removal

--- R/get_acoustic_detections.R
+++ R/get_acoustic_detections.R
@@ -13,7 +13,6 @@
 #' @param scientific_name Character (vector). One or more scientific names.
 #' @param acoustic_project_code Character (vector). One or more acoustic
 #'   project codes. Case-insensitive.
-#' @param deployment_id Character (vector). One or more deployment ids.
 #' @param receiver_id Character (vector). One or more receiver identifiers.
 #' @param station_name Character (vector). One or more deployment station
 #'   names.
