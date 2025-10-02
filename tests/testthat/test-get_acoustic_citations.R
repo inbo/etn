4a4888560bf7b4df0bddf7fbd6299acc0c425138
PietrH <pieterhuy@gmail.com>
2025-10-02 14:49:13 +0200

    Add test stubs

---
+++ tests/testthat/test-get_acoustic_citations.R
@@ -0,0 +1,7 @@
+test_that("get_acoustic_citations() can return multiple citations for a single acoustic_project_code", {
+
+})
+
+test_that("get_acoustic_citations() can group citations for multiple acoustic_project_codes", {
+
+})
