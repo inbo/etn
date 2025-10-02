000fcc0290de391a99d37eea89c6d919b9d3beb9
PietrH <pieterhuy@gmail.com>
2025-10-02 13:09:16 +0200

    Improve function docs.

--- tests/testthat/helper-expectations.R
+++ tests/testthat/helper-expectations.R
@@ -1,9 +1,21 @@
-#' Do functions in ETN return the same value over the API as with a query to a local database connection?
+#' Do functions in ETN return the same value over the API as with a query to a
+#' local database connection?
 #'
-#' This function tests if the provided expression returns identical results with the API flag set to TRUE or FALSE.
-#' To this it makes use of testthat's `expect_identical()`, no other parameters are passed to `expect_identical()`.
+#' This function tests if the provided expression returns identical regardless
+#' of the returned value of the internal helper `select_protocol()`. In other
+#' words, regardless of the way the data is received, data from a single source
+#' should be returned in exactly the same way.
 #'
-#' This expectation will be skipped when offline, or when the ETN database is not available via odbc.
+#' To test this this expectation makes use of testthat's `expect_identical()`
+#' and `with_mocked_bindings()` to temporarily override the return value of
+#' `select_protocol()` to simulate both API and local database calls. This
+#' expectation is useful to ensure that functions in ETN behave consistently
+#' regardless of the data source. It is particularly important for functions
+#' that can retrieve data from both an online API and a local database, as it
+#' helps to verify that the data handling and processing logic is robust and
+#' reliable across different data retrieval methods. This expectation will be
+#' skipped if the machine is offline or if the ETN database is not available.
+#'
 #'
 #' @param expression The expression to be tested.
 #' @inherits testthat::expect_identical return
