83b3e02818d09fef0b310da672cb960fef66fb37
PietrH <pieterhuy@gmail.com>
2025-10-02 15:25:00 +0200

    Format using cli, use purrr for list wrangling

--- R/get_acoustic_citations.R
+++ R/get_acoustic_citations.R
@@ -15,8 +15,12 @@
 get_acoustic_citations <- function(acoustic_project_code = NULL) {
   # Check if required packages are installed --------------------------------
   rlang::check_installed("jsonlite",
-                         reason = "To read metadata from the IMIS API")
+    reason = "To read metadata from the IMIS API"
+  )
 
+  rlang::check_installed("purrr",
+    reason = "For list wrangling, to help format the citations."
+  )
   # Query the IMIS dataset ids ----------------------------------------------
   imis_dataset_ids <-
     get_acoustic_projects(acoustic_project_code = acoustic_project_code) |>
@@ -26,10 +30,22 @@ get_acoustic_citations <- function(acoustic_project_code = NULL) {
 
   imis_metadata <-
     httr2::request("https://www.vliz.be/en/imis") |>
-    httr2::req_url_query(dasid = imis_dataset_ids,
-                         show = "json") |>
+    httr2::req_url_query(
+      dasid = imis_dataset_ids,
+      show = "json"
+    ) |>
     httr2::req_get_url() |>
     jsonlite::fromJSON()
 
 
+  # Parse the returned value ------------------------------------------------
+  citation_df <- imis_metadata[["dois"]]
+  citations <- dplyr::pull(citation_df, "Citation")
+
+  acronym <- purrr::chuck(imis_metadata, "datasetrec", "Acronym")
+  title <- purrr::chuck(imis_metadata, "datasetrec", "StandardTitle")
+  doi_urls <- glue::glue_col("https://doi.org/",
+                                   dplyr::pull(citation_df, "DOI"))
+  cli::cli_h2("{acronym} : {title}")
+  cli::cli_ul("{citations}. {.url {doi_urls}}")
 }
