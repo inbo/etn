2c283fc894cb6c8b9a869f2653670cf99912700c
PietrH <pieterhuy@gmail.com>
2025-10-02 14:54:38 +0200

    Read from the API using jsonlite

---
+++ R/get_acoustic_citations.R
@@ -0,0 +1,35 @@
+#' Get Acoustic Citations
+#'
+#' This function returns the the citations for all acoustic (= network) projects
+#' included in a dataset. These citations will be retrieved from
+#' [IMIS](https://www.vliz.be/nl/imis).
+#'
+#' @inheritParams get_acoustic_projects
+#'
+#' @returns (Invisible) A tibble with the citations for the specified acoustic
+#'   projects. And prints to console the formatted citations as shown on IMIS.
+#' @export
+#'
+#' @examples
+#' get_acoustic_citations("2004_Gudena")
+get_acoustic_citations <- function(acoustic_project_code = NULL) {
+  # Check if required packages are installed --------------------------------
+  rlang::check_installed("jsonlite",
+                         reason = "To read metadata from the IMIS API")
+
+  # Query the IMIS dataset ids ----------------------------------------------
+  imis_dataset_ids <-
+    get_acoustic_projects(acoustic_project_code = acoustic_project_code) |>
+    dplyr::pull("imis_dataset_id")
+
+  # Query the IMIS API ------------------------------------------------------
+
+  imis_metadata <-
+    httr2::request("https://www.vliz.be/en/imis") |>
+    httr2::req_url_query(dasid = imis_dataset_ids,
+                         show = "json") |>
+    httr2::req_get_url() |>
+    jsonlite::fromJSON()
+
+
+}
