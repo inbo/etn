7cd117bea248ddc2100a7b1e238be4a5a9631a26
pieter.huybrechts@inbo.be <pieter.huybrechts@inbo.be@rstudio4.web.vliz.be>
2026-01-20 13:44:20 +0000

    Explicitly declare empty column to be logical to be consistent

--- R/get_tags.R
+++ R/get_tags.R
@@ -45,6 +45,9 @@ get_tags <- function(connection,
     deprecate_warn_connection()
   }
   # Either use the API, or the SQL helper.
-  out <- conduct_parent_to_helpers(protocol = select_protocol())
+  out <- conduct_parent_to_helpers(protocol = select_protocol()) |>
+    # Set the column classes explicitly
+    dplyr::mutate(floating = as.logical(floating))
+
   return(out)
 }
