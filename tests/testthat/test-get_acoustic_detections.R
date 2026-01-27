4664ec146f79ca81703c73397d822d00c3f26820
PietrH <pieterhuy@gmail.com>
2026-01-27 14:28:10 +0100

    Try skipping on windows

--- tests/testthat/test-get_acoustic_detections.R
+++ tests/testthat/test-get_acoustic_detections.R
@@ -1,3 +1,5 @@
+testthat::skip_on_os("windows")
+
 test_that("get_acoustic_detections() can pass errors over the api", {
   # Test via the OpenCPU API
   withr::local_envvar(ETNSERVICE_PROTOCOL = "opencpu")
