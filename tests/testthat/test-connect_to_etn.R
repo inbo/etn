3d652d48891b0f63197da17537bf676cf8221314
PietrH <pieterhuy@gmail.com>
2026-02-03 17:16:37 +0100

    These tests do not add value.

--- tests/testthat/test-connect_to_etn.R
+++ tests/testthat/test-connect_to_etn.R
@@ -4,20 +4,6 @@ test_that("connect_to_etn() returns deprecation warning when used with named arg
   )
 })
 
-test_that("connect_to_etn() returns deprecation warning when used with unnamed arguments", {
-  skip("BUG #319: tests will always fail after first deprecation test")
-  lifecycle::expect_deprecated(
-    connect_to_etn("my name", "my password")
-  )
-})
-
-test_that("connect_to_etn() returns deprecation argument when used without arguments", {
-  skip("BUG #319: tests will always fail after first deprecation test")
-  lifecycle::expect_deprecated(
-    connect_to_etn()
-  )
-})
-
 test_that("connect_to_etn() returns NULL invisibly", {
   # connect_to_etn() is hard deprecated and no longer used
   # It should return NULL rather than connection information
