04358069da82278b1650fa8df5e1169bee0685e4
PietrH <pieterhuy@gmail.com>
2026-02-03 16:56:26 +0100

    Next release is major version

--- R/connect_to_etn.R
+++ R/connect_to_etn.R
@@ -1,6 +1,6 @@
 #' Connect to the ETN database
 #'
-#' This function is `r lifecycle::badge("deprecated")` since etn version 2.3.0.
+#' This function is `r lifecycle::badge("deprecated")` since etn version 3.0.0
 #' Its use is no longer supported or needed. All connections to the ETN database
 #' are now made automatically when you use a function. If your credentials are not saved
 #' in the system environement, you will be prompted to enter them.
