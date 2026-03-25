13d0a1095375d652f348bf9c44571ff3640a7eb3
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2026-03-25 11:07:29 +0100

    Create get_package.R
    Recyle from download_acoustic_dataset()

---
+++ R/get_package.R
@@ -0,0 +1,109 @@
+#' Get acoustic data package
+#'
+#' Get all acoustic data related to an **animal project**  as a
+#' **[Frictionless Data Package](https://specs.frictionlessdata.io/data-package/)**
+#' containing:
+#'
+#' file | description
+#' --- | ---
+#' `animals.csv` | Animals related to an `animal_project_code`, optionally filtered on `scientific_name`(s), as returned by `get_animals()`.
+#' `tags.csv` | Tags associated with the selected animals, as returned by `get_tags()`.
+#' `detections.csv` | Acoustic detections for the selected animals, as returned by `get_acoustic_detections()`.
+#' `deployments.csv` | Acoustic deployments for the `acoustic_project_code`(s) found in detections, as returned by `get_acoustic_deployments()`. This allows users to see when receivers were deployed, even if these did not detect the selected animals.
+#' `receivers.csv` | Acoustic receivers for the selected deployments, as returned by `get_acoustic_receivers()`.
+#' `datapackage.json` | A [Frictionless Table Schema](https://specs.frictionlessdata.io/table-schema/) metadata file describing the fields and relations of the above csv files. This file is copied from [here](https://github.com/inbo/etn/blob/master/inst/assets/datapackage.json) and can be used to validate the data package.
+#'
+#' **Important**: The data are downloaded _as is_ from the database, i.e. no
+#' quality or consistency checks are performed by this function. We therefore
+#' recommend to verify the data before publication. A consistency check can be
+#' performed by validation tools of the Frictionless Framework, e.g.
+#' `frictionless validate datapackage.json` on the command line using
+#' [frictionless-py](https://github.com/frictionlessdata/frictionless-py).
+#'
+#' @param animal_project_code Character. Animal project you want to get an
+#' acoustic data package for. Required.
+#'
+#' @return A Data Package object.
+#'
+#' @export
+#'
+#' @examplesIf etn:::credentials_are_set() & interactive()
+#' # Get data package for the 2012_leopoldkanaal animal project
+#' get_package(animal_project_code = "2012_leopoldkanaal")
+get_package <- function(animal_project_code) {
+  # Check animal_project_code
+  assertthat::assert_that(
+    length(animal_project_code) == 1,
+    msg = "`animal_project_code` must be a single value."
+  )
+  animal_project_code <- check_value(
+    animal_project_code,
+    list_animal_project_codes(),
+    "animal_project_code",
+    lowercase = TRUE
+  )
+
+  # ANIMALS
+  message("* (1/6): downloading animals.csv")
+  # Select on animal_project_code
+  animals <- get_animals(animal_project_code = animal_project_code)
+
+  # TAGS
+  message("* (2/6): downloading tags.csv")
+  # Select on tags associated with animals
+  tag_serial_numbers <-
+    animals |>
+    dplyr::distinct(.data$tag_serial_number) |>
+    dplyr::pull() |>
+    # To parse out multiple tags (e.g. "A69-9006-904,A69-9006-903"), combine
+    # all tags and split them again on comma
+    paste(collapse = ",") |>
+    strsplit("\\,") |>
+    unlist() |>
+    unique()
+  tags <- get_tags(tag_serial_number = tag_serial_numbers)
+
+  # DETECTIONS
+  message("* (3/6): downloading detections.csv")
+  # Select on animal_project_code
+  detections <- get_acoustic_detections(
+    animal_project_code = animal_project_code,
+    limit = FALSE
+  )
+  # Keep unique records
+  detections_orig_count <- nrow(detections)
+  detections <-
+    detections |>
+    dplyr::distinct(.data$detection_id, .keep_all = TRUE)
+
+  # DEPLOYMENTS
+  message("* (4/6): downloading deployments.csv")
+  # Select on acoustic_project_codes found in detections to get all deployments,
+  # including those without detections for animal_project_code
+  acoustic_project_codes <-
+    detections |>
+    dplyr::distinct(.data$acoustic_project_code) |>
+    dplyr::pull() |>
+    stringr::str_sort()
+  deployments <- get_acoustic_deployments(
+    acoustic_project_code = acoustic_project_codes,
+    open_only = FALSE
+  )
+  # Remove linebreaks in deployment comments to get single lines in csv:
+  deployments <-
+    deployments |>
+    dplyr::mutate(
+      comments = stringr::str_replace_all(.data$comments, "[\r\n]+", " ")
+    )
+
+  # RECEIVERS
+  message("* (5/6): downloading receivers.csv")
+  # Select on receivers associated with deployments
+  receiver_ids <-
+    deployments |>
+    dplyr::distinct(.data$receiver_id) |>
+    dplyr::pull()
+  receivers <- get_acoustic_receivers(
+    receiver_id = receiver_ids
+  )
+}
