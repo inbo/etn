78a037c39d3c5a7bbafbea2fb3269992abc1f2d3
PietrH <pieterhuy@gmail.com>
2026-04-03 15:51:10 +0200

    Port tests from etnservice

--- tests/testthat/test-get_receiver_logs.R
+++ tests/testthat/test-get_receiver_logs.R
@@ -73,11 +73,67 @@ test_that("get_receiver_logs() can filter on station_name", {
 })
 
 test_that("get_receiver_logs() can filter on start_date", {
+  expect_error(
+    get_receiver_logs(deployment_id = test_deployment_id,
+                      start_date = "not_a_date")
+  )
+
+  # Start date (inclusive) <= min(date_time)
+  start_year_df <- get_receiver_logs(deployment_id = test_deployment_id,
+                                     start_date = "2021")
+  expect_lte(as.POSIXct("2021-01-01", tz = "UTC"), min(start_year_df$datetime))
+  start_month_df <- get_receiver_logs(deployment_id = test_deployment_id,
+                                      start_date = "2020-09")
+  expect_lte(as.POSIXct("2020-09-01", tz = "UTC"), min(start_month_df$datetime))
+  start_day_df <- get_receiver_logs(deployment_id = test_deployment_id,
+                                    start_date = "2020-10-12")
+  expect_lte(as.POSIXct("2020-10-12", tz = "UTC"), min(start_day_df$datetime))
+
 
 })
 
 test_that("get_receiver_logs() can filter on end_date", {
+  expect_error(
+    get_receiver_logs(deployment_id = test_deployment_id,
+                      end_date = "not_a_date"),
+    regexp = "is not in a valid date format"
+  )
 
+  # End date (exclusive) > max(date_time)
+  end_year_df <- get_receiver_logs(end_date = "2021",
+                                   deployment_id = test_deployment_id)
+  expect_gt(as.POSIXct("2021-01-01", tz = "UTC"), max(end_year_df$datetime))
+  end_month_df <- get_receiver_logs(end_date = "2020-09",
+                                    deployment_id = test_deployment_id)
+  expect_gt(as.POSIXct("2020-09-01", tz = "UTC"), max(end_month_df$datetime))
+  end_day_df <- get_receiver_logs(end_date = "2021-02-10",
+                                  deployment_id = test_deployment_id)
+  expect_gt(as.POSIXct("2021-02-10", tz = "UTC"), max(end_day_df$datetime))
+})
+
+test_that("get_receiver_logs() can filter on both start and end date", {
+  # Test querying between two dates.
+  between_year_df <- get_receiver_logs(start_date = "2020",
+                                       end_date = "2021",
+                                       deployment_id = test_deployment_id)
+  expect_lte(as.POSIXct("2020-01-01", tz = "UTC"),
+             min(between_year_df$datetime))
+  expect_gt(as.POSIXct("2021-01-01", tz = "UTC"),
+            max(between_year_df$datetime))
+  between_month_df <- get_receiver_logs(start_date = "2020-09",
+                                        end_date = "2020-11",
+                                        deployment_id = test_deployment_id)
+  expect_lte(as.POSIXct("2020-09-01", tz = "UTC"),
+             min(between_month_df$datetime))
+  expect_gt(as.POSIXct("2020-11-01", tz = "UTC"),
+            max(between_month_df$datetime))
+  between_day_df <- get_receiver_logs(start_date = "2021-04-24",
+                                      end_date = "2021-04-25",
+                                      deployment_id = test_deployment_id)
+  expect_lte(as.POSIXct("2021-04-24", tz = "UTC"),
+             min(between_day_df$datetime))
+  expect_gt(as.POSIXct("2021-04-25", tz = "UTC"),
+            max(between_day_df$datetime))
 })
 
 test_that("get_receiver_logs() can return a limited subset", {
