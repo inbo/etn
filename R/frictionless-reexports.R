c8abb5dff489f83b81adaf2c436d88f4f33712ab
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2026-04-15 16:23:51 +0200

    Add family

--- R/frictionless-reexports.R
+++ R/frictionless-reexports.R
@@ -1,7 +1,9 @@
 #' @importFrom frictionless read_package
+#' @family frictionless functions
 #' @export
 frictionless::read_package
 
 #' @importFrom frictionless write_package
+#' @family frictionless functions
 #' @export
 frictionless::write_package
