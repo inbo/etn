f22920fdd97cc6cf64650be0679857346355e3ac
PietrH <pieterhuy@gmail.com>
2026-04-16 14:45:29 +0200

    Test child catalog fetching for return format

--- tests/testthat/test-utils-public.R
+++ tests/testthat/test-utils-public.R
@@ -12,7 +12,29 @@ test_that("read_catalog() returns expected list elements", {
 })
 
 # read_child_catalog() ----------------------------------------------------
+test_that("read_child_catalog() returns a list", {
+  expect_type(read_child_catalog(catalog = "metadata_files"), "list")
+  expect_type(read_child_catalog(catalog = "detection_files"), "list")
+  expect_type(read_child_catalog(catalog = "archival_files"), "list")
+})
 
+test_that("read_child_catalog() returns expected list elements", {
+  expect_named(
+    read_child_catalog(catalog = "metadata_files"),
+    c("type", "id", "stac_version", "description", "links", "extent",
+      "license")
+  )
+  expect_named(
+    read_child_catalog(catalog = "detection_files"),
+    c("type", "id", "stac_version", "description", "links", "extent",
+      "license")
+  )
+  expect_named(
+    read_child_catalog(catalog = "archival_files"),
+    c("type", "id", "stac_version", "description", "links", "extent",
+      "license")
+  )
+})
 
 # list_public_detections() ------------------------------------------------
 
