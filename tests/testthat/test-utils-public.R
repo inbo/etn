87b198afe152d2b029c24ff5883a7233f39533e5
PietrH <pieterhuy@gmail.com>
2026-04-16 14:23:55 +0200

    Add stub for type testing for catalog fetching

--- tests/testthat/test-utils-public.R
+++ tests/testthat/test-utils-public.R
@@ -1,6 +1,15 @@
 
 # read_catalog() ----------------------------------------------------------
+test_that("read_catalog() returns a list", {
+  expect_type(read_catalog(), "list")
+})
 
+test_that("read_catalog() returns expected list elements", {
+  expect_named(
+    read_catalog(),
+    c("type", "id", "stac_version", "description", "links", "title")
+  )
+})
 
 # read_child_catalog() ----------------------------------------------------
 
