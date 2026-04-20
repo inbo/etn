752ebc08dc84a12ac9685eb12fe749270a73cbf4
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2026-04-20 14:43:52 +0200

    Update test on dataset_id

--- tests/testthat/test-get_package.R
+++ tests/testthat/test-get_package.R
@@ -42,11 +42,11 @@ test_that("get_package() creates the expected package", {
   expect_snapshot_file(file.path(datapackage_path, "receivers.csv"))
 })
 
-# test_that("get_package() sets dataset_id when doi is missing", {
-#   skip_if_no_authentication()
-#
-#   vcr::local_cassette("package_bla")
-#
-#   package <- get_package() #TODO
-#   expect_equal(package$dataset_id, "https://marineinfo.org/id/dataset/")
-# })
+test_that("get_package() sets dataset_id when doi is missing", {
+  skip_if_no_authentication()
+
+  vcr::local_cassette("package_ESGL")
+
+  package <- get_package("ESGL")
+  expect_equal(package$id, "https://marineinfo.org/id/dataset/6291")
+})
