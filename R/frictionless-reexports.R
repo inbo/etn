7b1700544e5eeedf759d98249315ecbb30687288
Peter Desmet <peter.desmet@inbo.be>
2026-04-21 16:49:34 +0200

    Document parameters (avoid warning)

--- R/frictionless-reexports.R
+++ R/frictionless-reexports.R
@@ -1,6 +1,7 @@
 #' Read a Data Package descriptor file (`datapackage.json`)
 #'
 #' @importFrom frictionless read_package
+#' @inheritParams frictionless::read_package
 #' @family frictionless functions
 #' @rdname read_package
 #' @export
@@ -9,6 +10,7 @@ frictionless::read_package
 #' Write a Data Package to disk
 #'
 #' @importFrom frictionless write_package
+#' @inheritParams frictionless::write_package
 #' @family frictionless functions
 #' @rdname write_package
 #' @export
