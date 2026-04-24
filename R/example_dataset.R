c676dd51a6dc593cb098172a16e7043a1c9a5942
Peter Desmet <peter.desmet@inbo.be>
2026-04-24 09:51:51 +0200

    Describe how example_dataset was added to etn
    Changes:
    - Don't mention download_acoustic_dataset
    - Use lowercase directory name

--- R/example_dataset.R
+++ R/example_dataset.R
@@ -7,11 +7,15 @@
 #'
 #' `2014_DEMER` is a **river acoustic telemetry** dataset. It contains 66 tagged
 #' animals across four species. Over 235,000 detections were observed between
-#' 2014 and 2019 by acoustic receivers deployed in Belgian rivers.
+#' 2014 and 2019 by acoustic receivers deployed in Belgian rivers. Data are
+#' deposited at <https://doi.org/10.14284/432>.
 #'
-#' Data are deposited at <https://doi.org/10.14284/432> and can be downloaded
-#' from the ETN database with
-#' `download_acoustic_dataset(animal_project_code = "2014_DEMER")`.
+#' The dataset was included in the package with:
+#'
+#' ```R
+#' get_package(animal_project_code = "2014_demer") |>
+#'   write_package("inst/extdata/2014_demer")
+#' ```
 #'
 #' @param dataset Name of the example dataset to load. Defaults to the only
 #' available dataset: `"2014_DEMER"`.
