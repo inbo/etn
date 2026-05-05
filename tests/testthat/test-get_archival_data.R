11238623538c653406738297295378b5dea7020c
pieter.huybrechts@inbo.be <pieter.huybrechts@inbo.be@rstudio4.web.vliz.be>
2026-05-05 13:47:29 +0000

    Stub tests

---
+++ tests/testthat/test-get_archival_data.R
@@ -0,0 +1,27 @@
+test_that("get_archival_data() can filter on tag_serial_number", {
+
+})
+
+test_that("get_archival_data() returns a tibble", {
+
+})
+
+test_that("get_archival_data() returns the expected fields", {
+
+})
+
+test_that("get_archival_data() can filter on animal_id", {
+
+})
+
+test_that("get_archival_data() can filter on animal_project_code", {
+
+})
+
+test_that("get_archival_data() is case insensitive for animal_project_code", {
+
+})
+
+test_that("get_archival_data() returns error on invalid animal_project_code", {
+
+})
