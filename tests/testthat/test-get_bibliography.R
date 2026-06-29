518e9a217ad2ffb8c53e3ba6d4ccf64b91521ce0
PietrH <pieterhuy@gmail.com>
2026-06-29 10:41:15 +0200

    Stub tests from spec mentioned in issue

---
+++ tests/testthat/test-get_bibliography.R
@@ -0,0 +1,38 @@
+test_that("get_bibliography() accepts dataframes with expected columns", {
+  
+})
+
+test_that("get_bibliography() returns error on missing columns", {
+
+})
+
+test_that("get_bibliography() returns error on unexpected input type", {
+
+})
+
+test_that("get_bibliography() handles missing project codes", {
+
+})
+
+test_that("get_bibliography() returns data.frame with expeted columns", {
+
+})
+
+test_that("get_bibliography() returns data.frame with expected shape", {
+  #  One row for the ETN data system, with a hardcoded citation
+  # One row for the etn R package, with a citation to the latest (non-dev) release
+  # One row for each animal project
+  # One row for each acoustic project
+})
+
+test_that("get_bibliography() returns data.frame hardcoded etn citation", {
+
+})
+
+test_that("get_bibliography() returns package citation", {
+
+})
+
+test_that("get_bibliography() returns animal and project citations", {
+
+})
