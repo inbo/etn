09d972e24f129f893333bf36add9ef6cd018176c
PietrH <pieterhuy@gmail.com>
2026-06-29 10:32:30 +0200

    Reword
    Co-Authored-By: Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
    Co-Authored-By: Peter Desmet <peter.desmet@inbo.be>

--- R/write_dwc.R
+++ R/write_dwc.R
@@ -43,7 +43,7 @@
 #'   Sex, life stage and weight are (additionally) provided in an Extended
 #'   Measurement Or Facts extension, where values are mapped to a controlled
 #'   vocabulary recommended by [OBIS](https://obis.org/).
-#' - Acoustic detections are downsampled to the **first detection per hour**,
+#' - Acoustic detections are subsampled to the **first detection per hour**,
 #'   to reduce the size of high-frequency data.
 #'   The `coordinateUncertaintyInMeters` is set to 1000 m to account for
 #'   imprecise receiver location and acoustic detection range.
