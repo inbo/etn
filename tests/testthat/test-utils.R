dd5872aec78e770b52b07312885aa6678de1803e
PietrH <pieterhuy@gmail.com>
2026-06-30 16:46:06 +0200

    Test for presence of authors: work around issue of periods and commas changing depending on OS.
    Co-Authored-By: Peter Desmet <peter.desmet@inbo.be>

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -89,14 +89,8 @@ test_that("etn_citation() returns a character vector", {
 })
 
 test_that("etn_citation() returns the expected package citation", {
-  expect_identical(
+  expect_match(
     etn_citation(),
-    paste0(
-      "Huybrechts P, Desmet P, Govaert S, Oldoni D, Van Hoey S (2026).",
-      " etn: Access Data from the European Tracking Network.",
-      " https://doi.org/10.5281/zenodo.15235747, R package version ",
-      packageVersion("etn"),
-      ", https://inbo.github.io/etn/."
-    )
+    "Huybrechts P, Desmet P, Govaert S, Oldoni D, Van Hoey S"
   )
 })
