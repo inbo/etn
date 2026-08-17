9123d25e4d92acbb469fcf8496a5c25a7d3799b0
PietrH <pieterhuy@gmail.com>
2026-08-17 10:10:26 +0200

    Add code family for pkgdown

--- R/get_archival_data.R
+++ R/get_archival_data.R
@@ -47,7 +47,7 @@
 #'  argument. See [arrow::Dataset] for more information.
 #'
 #' @export
-#'
+#' @family access functions
 #' @examplesIf interactive() & credentials_are_set()
 #'   get_archival_data(tag_serial_number = "A15757", limit = TRUE)
 get_archival_data <- function(tag_serial_number = NULL,
