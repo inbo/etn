73cd1880e2ee369a2e8a509c70949589006a6db5
PietrH <pieterhuy@gmail.com>
2026-08-18 17:02:03 +0200

    Reduce size of request to speed test up.

--- tests/testthat/test-get_archival_data.R
+++ tests/testthat/test-get_archival_data.R
@@ -4,7 +4,8 @@ test_that("get_archival_data() returns a tibble by default", {
   skip_if_no_authentication()
 
   expect_s3_class(
-    get_archival_data(limit = TRUE),
+    get_archival_data(tag_serial_number = "A15757",
+                      limit = TRUE),
     "tbl"
   )
 })
@@ -15,8 +16,11 @@ test_that("get_archival_data() can return an arrow datasetquery", {
   skip_if_no_authentication()
 
   expect_s3_class(
-    get_archival_data(return_as = "arrow",
-                      limit = TRUE),
+    get_archival_data(
+      tag_serial_number = "A15757",
+      return_as = "arrow",
+      limit = TRUE
+    ),
     "arrow_dplyr_query"
   )
 })
