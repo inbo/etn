c7abb6cce20ab7d804a15c6a8dd02ed63e0a143a
PietrH <pieterhuy@gmail.com>
2026-08-20 12:47:35 +0200

    purple candidates
    Co-Authored-By: Peter Desmet <peter.desmet@inbo.be>

--- R/utils.R
+++ R/utils.R
@@ -60,7 +60,7 @@ check_value <- function(x, y, name = "value", lowercase = FALSE, max_dist = 3) {
     # If there are many candidates, truncate the list to 5 items for the error
     # message.
     cli::cli_abort(
-      "Can't find {.var {name}}: {.val {missing_values}} in: {.or {.str {candidates_col}}}.
+      "Can't find {.var {name}}: {.val {missing_values}} in: {.or {.val {candidates_col}}}.
       Did you mean {.strong {.val {closest_match}}}?",
       class = "etn_value_not_found_suggest",
       call = rlang::caller_env()
